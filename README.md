cdbff4c0d6bfa23012880d4ea943701a0ad86232
tcbegley <tom@asidatascience.com>
2018-09-21 14:50:05 +0100

    Expand README with basic usage, add long_description to setup.py

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 # dash-bootstrap-components
 
-This package provides bootstrap components for use in Plotly Dash
+This package provides bootstrap components for use in Plotly Dash.
 
+## Installation
+
+`dash-bootstrap-components` is hosted on PyPI, and can be installed by running
+
+```
+pip install dash-bootstrap-components
+```
+
+## Usage
+
+`dash-bootstrap-components` does not come bundled with Bootstrap CSS to style
+the components. This is so that users are able to compile Bootstrap CSS
+themselves and override defaults to acheive the styling they want. Consequently
+when using `dash-bootstrap-components` in your dashboard, you must link the CSS
+you wish to use yourself. There are numerous ways to do this, the simplest is to
+create a folder called `assets` in the root of your app directory and add the
+Bootstrap CSS file. Your Dash app will then automatically serve and link any
+stylesheets saved in this directory.
+
+For more information, or alternative ways to link CSS, check the Plotly Dash
+[user guide](https://dash.plot.ly/external-resources).
+
+Documentation of the components available in `dash-bootstrap-components` is
+upcoming, for now you can see some examples in the `examples directory`.
--- setup.py
+++ setup.py
@@ -8,21 +8,29 @@ HERE = os.path.dirname(os.path.abspath(__file__))
 def _get_version():
     """ Get version by parsing _version programmatically """
     version_ns = {}
-    with open(os.path.join(HERE, "dash_bootstrap_components", "_version.py")) as f:
+    with open(
+        os.path.join(HERE, "dash_bootstrap_components", "_version.py")
+    ) as f:
         exec(f.read(), {}, version_ns)
     version = version_ns["__version__"]
     return version
 
 
+def _get_long_description():
+    with open(os.path.join(HERE, "README.md")) as f:
+        return f.read()
+
+
 setup(
     name="dash-bootstrap-components",
     version=_get_version(),
-    description="""
-Bootstrap themed components for use in Plotly Dash
-    """,
+    description="Bootstrap themed components for use in Plotly Dash",
+    long_description=_get_long_description(),
+    long_description_content_type="text/markdown",
+    license="Apache Software License",
     author="ASI Data Science",
     author_email="engineering@asidatascience.com",
-    url="https://www.asidatascience.com/",
+    url="https://github.com/ASIDataScience/dash-bootstrap-components",
     packages=find_packages(),
     install_requires=["dash"],
     include_package_data=True,
