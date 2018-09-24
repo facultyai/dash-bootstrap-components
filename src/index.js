80e66b84d153614009b008dedf16c3d028f80ac7
tcbegley <tom@asidatascience.com>
2018-09-24 18:56:38 +0100

    Adding Tooltip component

--- src/index.js
+++ src/index.js
@@ -25,3 +25,4 @@ export { default as PopoverBody } from './components/PopoverBody';
 export { default as PopoverHeader } from './components/PopoverHeader';
 export { default as Row } from './components/layout/Row';
 export { default as Table } from './components/Table';
+export { default as Tooltip } from './components/Tooltip';
