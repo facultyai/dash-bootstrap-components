0c482797005942aac3515db6a66e8baaa61f44f3
Pascal Bugnion <pascal@bugnion.org>
2018-09-29 11:04:54 +0100

    Add JSDOM

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,12 @@
-// setup file
+// enzyme
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+// jsdom
+import {JSDOM} from 'jsdom';
+
+const documentHTML = '<!doctype html><html><body><div id="root"></div></body></html>';
+global.document = new JSDOM(documentHTML);
+global.window = document.parentWindow;
