f3b6eb51738ea214ce9dc4f6ed39dc081e861fba
Pascal Bugnion <pascal@bugnion.org>
2018-09-29 09:17:46 +0100

    Add initial unit testing framework

---
+++ src/components/base/__tests__/DashLink.test.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import {shallow} from 'enzyme';
+import DashLink from '../DashLink'
+
+describe('test', () => {
+  it('work', () => {
+    const link = shallow(<DashLink>hello</DashLink>)
+  })
+})
