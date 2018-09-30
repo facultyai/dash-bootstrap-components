afb731e6721f738cf5db93753699bd028a4bd2ad
Pascal Bugnion <pascal@bugnion.org>
2018-09-30 08:56:21 +0100

    Avoid configuring JSDOM in test setup

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,9 +4,3 @@ import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
-// jsdom
-import {JSDOM} from 'jsdom';
-
-const documentHTML = '<!doctype html><html><body><div id="root"></div></body></html>';
-global.document = new JSDOM(documentHTML);
-global.window = document.parentWindow;
