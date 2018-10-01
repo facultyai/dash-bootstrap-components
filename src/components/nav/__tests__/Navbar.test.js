a6b3cc3ab6770a90014c9e2e683e734569945f96
Pascal Bugnion <pascal@bugnion.org>
2018-10-01 07:25:36 +0100

    Test that we actually build a reactstrap Navbar

--- src/components/nav/__tests__/Navbar.test.js
+++ src/components/nav/__tests__/Navbar.test.js
@@ -1,12 +1,14 @@
 
 import React from 'react';
 import {shallow} from 'enzyme';
+import {Navbar as RSNavbar} from 'reactstrap';
 import Navbar from '../Navbar';
 import NavbarBrand from '../NavbarBrand';
 import NavItem from '../NavItem';
 
 describe('Empty Navbar', () => {
   let navbar;
+  let rsNavbar;
   let navbarBrand;
 
   beforeAll(() => {
@@ -15,8 +17,13 @@ describe('Empty Navbar', () => {
         <NavItem>item</NavItem>
       </Navbar>
     );
+    rsNavbar = navbar.find(RSNavbar)
     navbarBrand = navbar.find(NavbarBrand);
   })
+
+  it('should contain a reactstrap Navbar', () =>
+    expect(rsNavbar).toHaveLength(1)
+  )
 
   it('should not have a brand', () =>
     expect(navbarBrand).toHaveLength(0)
