d0fdbc61621badfdeff2d0d749ab876baadee846
tcbegley <tom@asidatascience.com>
2018-10-03 18:23:48 +0100

    Allow links in DropdownItem

--- src/components/DropdownItem.js
+++ src/components/DropdownItem.js
@@ -1,14 +1,33 @@
 import PropTypes from 'prop-types';
 import {DropdownItem as RSDropdownItem} from 'reactstrap';
+import Link from '../private/Link';
+import Button from './Button';
 
 const DropdownItem = props => {
   const {
     children,
+    href,
     ...otherProps
   } = props;
-  return (<RSDropdownItem {...otherProps}>
-    {children}
-  </RSDropdownItem>);
+  return (
+    <RSDropdownItem
+    onClick={() => {
+      if (props.setProps) {
+        props.setProps({
+          n_clicks: props.n_clicks + 1,
+          n_clicks_timestamp: Date.now()
+        })
+      }
+      if (props.fireEvent)
+        props.fireEvent({event: 'click'});
+      }
+    }
+    tag={href ? Link : 'button'}
+    href={href}
+    {...otherProps}>
+      {children}
+    </RSDropdownItem>
+  );
 }
 
 DropdownItem.propTypes = {
@@ -57,7 +76,42 @@ DropdownItem.propTypes = {
   /**
    * Link for the menu item
    */
-  href: PropTypes.string
+  href: PropTypes.string,
+
+  /**
+   * If true, the browser will treat this as an external link,
+   * forcing a page refresh at the new location. If false,
+   * this just changes the location without triggering a page
+   * refresh. Use this if you are observing dcc.Location, for
+   * instance. Defaults to true for absolute URLs and false
+   * otherwise.
+   */
+  external_link: PropTypes.bool,
+
+  /**
+   * An integer that represents the number of times
+   * that this element has been clicked on.
+   */
+  n_clicks: PropTypes.number,
+
+  /**
+   * An integer that represents the time (in ms since 1970)
+   * at which n_clicks changed. This can be used to tell
+   * which button was changed most recently.
+   */
+  n_clicks_timestamp: PropTypes.number,
+
+  /**
+   * A callback for firing events to dash.
+   */
+  fireEvent: PropTypes.func,
+
+  dashEvents: PropTypes.oneOf(['click'])
+};
+
+DropdownItem.defaultProps = {
+  n_clicks: 0,
+  n_clicks_timestamp: -1
 };
 
 export default DropdownItem;
