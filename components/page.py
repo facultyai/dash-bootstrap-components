441baa6f3787bfdd1b6f512cb27fc428fea61971
Pascal Bugnion <pascal@bugnion.org>
2018-10-06 10:28:47 +0100

    Restructure components docs page for modularity

--- components/page.py
+++ components/page.py
@@ -26,12 +26,49 @@ header = html.Div(
     ]
 )
 
-components_page = dbc.Container([
-    header,
-    alerts,
-    dcc.SyntaxHighlighter(alerts_source, language='python', theme='dark'),
-    html.Br(),
-    badges,
-    dcc.SyntaxHighlighter(badges_source, language='python', theme='dark'),
-    html.Br()
-])
+sidebar = [
+    html.H1('Components', className='h5'),
+    dbc.Nav(
+        [
+            dbc.NavItem(dbc.NavLink('Alerts', href='/l/components/alerts')),
+            dbc.NavItem(dbc.NavLink('Badges', href='/l/components/badges')),
+        ],
+        className='flex-column'
+    )
+]
+
+
+def component_container(contents):
+    return [header] + contents
+
+
+def component_page(contents):
+    return dbc.Container(
+        dbc.Row([
+            dbc.Col(component_container(contents), md=9),
+            dbc.Col(sidebar, md=3)
+        ])
+    )
+
+
+contents = {
+    'alerts': [
+        alerts,
+        dcc.SyntaxHighlighter(alerts_source, language='python', theme='dark')
+    ],
+    'badges': [
+        badges,
+        dcc.SyntaxHighlighter(badges_source, language='python', theme='dark')
+    ]
+}
+
+
+class ComponentsPage:
+
+    def for_path(self, path_components):
+        try:
+            component_name = path_components[0]
+            component_contents = contents[component_name]
+            return component_page(component_contents)
+        except IndexError:
+            return self.for_path(['alerts'])
