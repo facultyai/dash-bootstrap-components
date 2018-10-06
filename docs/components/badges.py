ecdd66965538061a0f1f12a11b6107d0399655b4
Pascal Bugnion <pascal@bugnion.org>
2018-10-06 08:16:03 +0100

    Add badges to components list

---
+++ docs/components/badges.py
@@ -0,0 +1,14 @@
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+
+badges = html.Div(
+    [
+        html.H2("Badges"),
+        html.H3(
+            [
+                "This is a heading with a badge!",
+                dbc.Badge("New!", color="success"),
+            ]
+        ),
+    ]
+)
--- docs/components/page.py
+++ docs/components/page.py
@@ -6,10 +6,12 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc
 
 from .alerts import alerts
+from .badges import badges
 
 HERE = Path(__file__).parent
 
-alerts_source = (HERE / 'alerts.py').open().read()
+alerts_source = (HERE / "alerts.py").open().read()
+badges_source = (HERE / "badges.py").open().read()
 
 
 header = html.Div(
@@ -28,5 +30,8 @@ components_page = dbc.Container([
     header,
     alerts,
     dcc.SyntaxHighlighter(alerts_source, language='python', theme='dark'),
+    html.Br(),
+    badges,
+    dcc.SyntaxHighlighter(badges_source, language='python', theme='dark'),
     html.Br()
 ])
