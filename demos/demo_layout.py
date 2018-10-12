327999b02fc6a19c5bfe8f71664112b8492c67fe
Pascal Bugnion <pascal@bugnion.org>
2018-10-12 09:19:11 +0100

    Example application

--- demos/demo_layout.py
+++ demos/demo_layout.py
@@ -1,8 +1,31 @@
 import dash_bootstrap_components as dbc
+import dash_core_components as dcc
 import dash_html_components as html
 
 _body = dbc.Container(
-    html.P('Some text')
+    [
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        html.H2('Heading'),
+                        html.P('Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.'),
+                        dbc.Button('View details', color='secondary')
+                    ],
+                    md=4
+                ),
+                dbc.Col(
+                    [
+                        html.H2('Graph'),
+                        dcc.Graph(
+                            id='dash-docs-graph',
+                            figure={'data': [{'x': [1, 2, 3], 'y': [1, 4, 9]}]}
+                        )
+                    ]
+                )
+            ]
+        )
+    ]
 )
 
 
