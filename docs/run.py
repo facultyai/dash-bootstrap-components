dadbffa38a6e14369f17b29f19b30e41952627e5
Pascal Bugnion <pascal@bugnion.org>
2018-10-14 10:51:42 +0100

    Remove obsolete lines

--- docs/run.py
+++ docs/run.py
@@ -1,11 +1,6 @@
-import dash
-import dash_html_components as html
-import dash_bootstrap_components as dbc
 from flask import Flask, render_template
 
 from app import App
-
-# from components import components_page
 
 
 server = Flask(__name__)
@@ -20,4 +15,3 @@ App.from_server(server, "/l/")
 
 if __name__ == "__main__":
     server.run(debug=True, port=8888)
-    # app.run_server(debug=True, threaded=True, port=8888)
