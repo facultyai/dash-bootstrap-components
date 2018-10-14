6fc177c6b6040fcddb50fe09fffbea2508498cca
Pascal Bugnion <pascal@bugnion.org>
2018-10-14 08:31:27 +0100

    Add task for a full release

--- tasks.py
+++ tasks.py
@@ -1,5 +1,7 @@
 
 import os
+import tempfile
+from subprocess import call
 from pathlib import Path
 
 from termcolor import cprint
@@ -9,6 +11,12 @@ import semver
 VERSION_TEMPLATE = """
 __version__ = "{version_string}"
 """
+
+RELEASE_NOTES_TEMPLATE = '''# Write the release notes here
+# Delete the version title to cancel
+Version {version_string}
+{underline}
+'''
 
 HERE = Path(__file__).parent
 
@@ -25,6 +33,37 @@ def prerelease(ctx, version):
      - Push a release to pypi and npm
     '''
     info(f"Releasing version {version} as prerelease")
+    build_publish(version)
+
+
+@task(help={'version': 'Version number to release'})
+def release(ctx, version):
+    '''
+    Release a new version
+    Running this task will:
+     - Prompt the user for a changelog and write it to
+       the release notes
+     - Commit the release notes
+     - Bump the version number
+     - Push a release to pypi and npm
+    '''
+    info(f"Releasing version {version} as full release")
+    release_notes_lines = get_release_notes(version)
+
+    if release_notes_lines is None:
+        error('No release notes: exiting')
+        exit()
+
+    info("Writing release notes to changelog.tmp")
+    with open("changelog.tmp", "w") as f:
+        f.writelines(release_notes_lines)
+
+    # TODO when we have release notes, these should be amended here
+
+    build_publish(version)
+
+
+def build_publish(version):
     info("Cleaning")
     clean()
     info("Updating versions")
@@ -79,6 +118,39 @@ def set_jsversion(version):
             package_json[iline] = '  "version": "{}",\n'.format(version)
     with open(package_json_path, 'w') as f:
         f.writelines(package_json)
+
+
+def get_release_notes(version):
+    version = normalize_version(version)
+    underline = '=' * len('Version {}'.format(version))
+    initial_message = RELEASE_NOTES_TEMPLATE.format(
+        version_string=version, underline=underline)
+    lines = open_editor(initial_message)
+    non_commented_lines = [line for line in lines if not line.startswith('#')]
+    changelog = ''.join(non_commented_lines)
+    if version in changelog:
+        if not non_commented_lines[-1].isspace():
+            non_commented_lines.append('\n')
+        return non_commented_lines
+    else:
+        return None
+
+
+def open_editor(initial_message):
+    editor = os.environ.get('EDITOR', 'vim')
+    tmp = tempfile.NamedTemporaryFile(suffix='.tmp')
+    fname = tmp.name
+
+    with open(fname, 'w') as f:
+        f.write(initial_message)
+        f.flush()
+
+    call([editor, fname], close_fds=True)
+
+    with open(fname, 'r') as f:
+        lines = f.readlines()
+
+    return lines
 
 
 def normalize_version(version):
