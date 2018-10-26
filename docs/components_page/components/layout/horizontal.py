c0a621c1ae16ce7f6427252141e1a40ee8783a24
tcbegley <tom@asidatascience.com>
2018-10-26 16:12:09 +0100

    Add horizontal alignment examples

---
+++ docs/components_page/components/layout/horizontal.py
@@ -0,0 +1,43 @@
+import dash_bootstrap_components as dbc
+import dash_html_components as html
+
+row = html.Div(
+    [
+        html.H4("Horizontal alignment"),
+        dbc.Row(
+            [
+                dbc.Col(html.Div("One of two columns"), width=4),
+                dbc.Col(html.Div("One of two columns"), width=4),
+            ],
+            justify="start",
+        ),
+        dbc.Row(
+            [
+                dbc.Col(html.Div("One of two columns"), width=4),
+                dbc.Col(html.Div("One of two columns"), width=4),
+            ],
+            justify="center",
+        ),
+        dbc.Row(
+            [
+                dbc.Col(html.Div("One of two columns"), width=4),
+                dbc.Col(html.Div("One of two columns"), width=4),
+            ],
+            justify="end",
+        ),
+        dbc.Row(
+            [
+                dbc.Col(html.Div("One of two columns"), width=4),
+                dbc.Col(html.Div("One of two columns"), width=4),
+            ],
+            justify="between",
+        ),
+        dbc.Row(
+            [
+                dbc.Col(html.Div("One of two columns"), width=4),
+                dbc.Col(html.Div("One of two columns"), width=4),
+            ],
+            justify="around",
+        ),
+    ]
+)
