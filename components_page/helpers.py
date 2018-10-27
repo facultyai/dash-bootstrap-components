797dd802fc6f2b2c7450c1f2634b086e70e4dbb7
Pascal Bugnion <pascal@bugnion.org>
2018-10-27 10:43:14 +0100

    Style reference documentation

--- components_page/helpers.py
+++ components_page/helpers.py
@@ -1,5 +1,6 @@
 import dash_html_components as html
 import dash_core_components as dcc
+from dash.development.base_component import js_to_py_type
 
 
 def HighlightedSource(source):
@@ -29,20 +30,33 @@ def load_source_with_app(app, source, component_name):
 
 def ApiDoc(component_metadata):
     return html.Div([
-        html.H4("API", className="mt-5"),
+        html.H4("API reference", className="mt-5 mb-2"),
         ArgumentsList(component_metadata)
-    ])
+    ], className="api-documentation")
 
 
 def ArgumentsList(component_metadata):
     props_metadata = component_metadata.get("props", {})
-    arguments = [
-        Argument(name, metadata)
-        for name, metadata in props_metadata.items()
-    ]
-    return html.Div(arguments)
+    arguments = []
+    for name, metadata in props_metadata.items():
+        arguments.append(Argument(name, metadata))
+    return html.Ul(arguments, className="list-unstyled")
 
 
 def Argument(argument_name, argument_metadata):
     description = argument_metadata.get("description", "")
-    return html.Div(f"{argument_name}: {description}")
+    required = (
+        ""
+        if argument_metadata.get("required", False)
+        else ", optional"
+    )
+    argument_type = argument_metadata.get("type", {})
+    argument_type = js_to_py_type(argument_type)
+    if argument_type is not None:
+        type_string = f" ({argument_type}{required})"
+    else:
+        type_string = ""
+    return html.Li([
+        html.Code(argument_name), html.I(type_string), ": ",
+        description
+    ])
