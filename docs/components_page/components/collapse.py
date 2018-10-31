23c8930c1e75a89d7fe954bf7277b7c529880352
tcbegley <tom@asidatascience.com>
2018-10-31 23:17:24 +0000

    Update docs with snake case attributes

--- docs/components_page/components/collapse.py
+++ docs/components_page/components/collapse.py
@@ -19,9 +19,9 @@ collapse = html.Div(
 
 
 @app.callback(
-    Output("collapse", "isOpen"),
+    Output("collapse", "is_open"),
     [Input("collapse-button", "n_clicks")],
-    [State("collapse", "isOpen")],
+    [State("collapse", "is_open")],
 )
 def toggle_collapse(n, is_open):
     if n:
