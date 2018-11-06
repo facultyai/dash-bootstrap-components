537f0b9d15beaaf263620861474bd454bde61b49
tcbegley <tom@asidatascience.com>
2018-11-06 07:15:38 +0000

    Add inline form

--- docs/components_page/forms_content.py
+++ docs/components_page/forms_content.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from .api_doc import ApiDoc
 from .components.forms.grid import form as form_grid
+from .components.forms.inline import form as form_inline
 from .components.forms.radio_check import form as form_radio_check
 from .components.forms.row import form as form_row
 from .components.forms.simple import form as form_simple
@@ -15,6 +16,7 @@ form_simple_source = (FORMS / "simple.py").open().read()
 form_radio_check_source = (FORMS / "radio_check.py").open().read()
 form_row_source = (FORMS / "row.py").open().read()
 form_grid_source = (FORMS / "grid.py").open().read()
+form_inline_source = (FORMS / "inline.py").open().read()
 
 content = [
     ExampleContainer(form_simple),
@@ -25,6 +27,8 @@ content = [
     HighlightedSource(form_row_source),
     ExampleContainer(form_grid),
     HighlightedSource(form_grid_source),
+    ExampleContainer(form_inline),
+    HighlightedSource(form_inline_source),
     ApiDoc(
         get_component_metadata("src/components/form/Form.js"),
         component_name="Form",
