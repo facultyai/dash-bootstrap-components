cc77a563bd86d1449ed4fe850ec05117c94d9b42
tcbegley <tom@asidatascience.com>
2018-11-06 07:21:26 +0000

    Add dash core components

--- components_page/forms_content.py
+++ components_page/forms_content.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 
+import dash_html_components as html
+
 from .api_doc import ApiDoc
+from .components.forms.dcc import form as form_dcc
 from .components.forms.grid import form as form_grid
 from .components.forms.inline import form as form_inline
 from .components.forms.radio_check import form as form_radio_check
@@ -17,6 +20,7 @@ form_radio_check_source = (FORMS / "radio_check.py").open().read()
 form_row_source = (FORMS / "row.py").open().read()
 form_grid_source = (FORMS / "grid.py").open().read()
 form_inline_source = (FORMS / "inline.py").open().read()
+form_dcc_source = (FORMS / "dcc.py").open().read()
 
 content = [
     ExampleContainer(form_simple),
@@ -29,6 +33,11 @@ content = [
     HighlightedSource(form_grid_source),
     ExampleContainer(form_inline),
     HighlightedSource(form_inline_source),
+    html.P(
+        "The Form and FormGroup components work well with Dash core components"
+    ),
+    ExampleContainer(form_dcc),
+    HighlightedSource(form_dcc_source),
     ApiDoc(
         get_component_metadata("src/components/form/Form.js"),
         component_name="Form",
