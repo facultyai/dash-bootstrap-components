8fa2568fca1da66d33d126b4a30e47ddb93e0cf0
tcbegley <tom@asidatascience.com>
2018-11-06 06:57:44 +0000

    Add RadioItems and Checklist to forms documentation

--- docs/components_page/forms_content.py
+++ docs/components_page/forms_content.py
@@ -1,31 +1,33 @@
 from pathlib import Path
 
 from .api_doc import ApiDoc
+from .components.forms.radio_check import form as form_radio_check
 from .components.forms.simple import form as form_simple
 from .helpers import ExampleContainer, HighlightedSource
 from .metadata import get_component_metadata
-
-# from .components.cards.simple import cards as cards_simple
-# from .components.cards.content_types import cards as cards_content_types
-# from .components.cards.group import cards as cards_group
-# from .components.cards.columns import cards as cards_columns
-
 
 HERE = Path(__file__).parent
 FORMS = HERE / "components" / "forms"
 
 form_simple_source = (FORMS / "simple.py").open().read()
-# cards_simple_source = (CARDS / "simple.py").open().read()
-# cards_content_type_source = (CARDS / "content_types.py").open().read()
-# cards_group_source = (CARDS / "group.py").open().read()
-# cards_columns_source = (CARDS / "columns.py").open().read()
-
+form_radio_check_source = (FORMS / "radio_check.py").open().read()
 
 content = [
     ExampleContainer(form_simple),
     HighlightedSource(form_simple_source),
+    ExampleContainer(form_radio_check),
+    HighlightedSource(form_radio_check_source),
     ApiDoc(
         get_component_metadata("src/components/form/Form.js"),
         component_name="Form",
+    ),
+    ApiDoc(get_component_metadata("src/components/form/FormGroup.js")),
+    ApiDoc(
+        get_component_metadata("src/components/input/RadioItems.js"),
+        component_name="RadioItems",
+    ),
+    ApiDoc(
+        get_component_metadata("src/components/input/Checklist.js"),
+        component_name="Checklist",
     ),
 ]
