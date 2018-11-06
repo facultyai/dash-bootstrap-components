b58546d86c1b7684b15fbe8edf0ba91f57c78574
tcbegley <tom@asidatascience.com>
2018-11-06 06:47:58 +0000

    Add simple form example

--- docs/components_page/page.py
+++ docs/components_page/page.py
@@ -7,6 +7,7 @@ from .badges_content import content as badges_content
 from .buttons_content import get_content as get_buttons_content
 from .cards_content import content as cards_content
 from .collapse_content import get_content as get_collapse_content
+from .forms_content import content as forms_content
 from .layout_content import content as layout_content
 
 from .sidebar import Sidebar, SidebarEntry
@@ -32,6 +33,7 @@ sidebar_entries = [
     SidebarEntry("buttons", "Buttons"),
     SidebarEntry("cards", "Cards"),
     SidebarEntry("collapse", "Collapse"),
+    SidebarEntry("forms", "Forms"),
     SidebarEntry("layout", "Layout"),
 ]
 
@@ -55,6 +57,7 @@ class ComponentsPage:
             "buttons": get_buttons_content(self._app),
             "cards": cards_content,
             "collapse": get_collapse_content(self._app),
+            "forms": forms_content,
             "layout": layout_content,
         }
 
