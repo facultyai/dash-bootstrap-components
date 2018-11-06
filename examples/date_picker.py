55a6f5e83774564951c046ef4037fb0b5957d8fd
tcbegley <tom@asidatascience.com>
2018-11-06 17:44:42 +0000

    example app for testing

--- examples/date_picker.py
+++ examples/date_picker.py
@@ -2,11 +2,21 @@ from datetime import datetime
 
 import dash
 import dash_bootstrap_components as dbc
+import dash_html_components as html
 
 app = dash.Dash()
 
 app.layout = dbc.Container(
-    dbc.DatePickerSingle(initial_visible_month=datetime(2018, 1, 1))
+    [
+        html.H1("Date picker testing"),
+        dbc.DatePickerSingle(initial_visible_month=datetime(2018, 1, 1)),
+        dbc.DatePickerSingle(
+            initial_visible_month=datetime(2018, 1, 1), bs_size="sm"
+        ),
+        dbc.DatePickerSingle(
+            initial_visible_month=datetime(2018, 1, 1), bs_size="lg"
+        ),
+    ]
 )
 
 
