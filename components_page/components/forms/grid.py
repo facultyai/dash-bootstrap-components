bedf0285abbba4a466dd59f34ea98c7bb4ea24b1
tcbegley <tom@asidatascience.com>
2018-11-06 07:09:51 +0000

    Add form grid example

---
+++ components_page/components/forms/grid.py
@@ -0,0 +1,38 @@
+import dash_bootstrap_components as dbc
+import dash_html_components as html
+
+grid = dbc.Row(
+    [
+        dbc.Col(
+            dbc.FormGroup(
+                [
+                    dbc.Label("Email", html_for="example-email-grid"),
+                    dbc.Input(
+                        type="email",
+                        id="example-email-grid",
+                        placeholder="Enter email",
+                    ),
+                ]
+            ),
+            width=6,
+        ),
+        dbc.Col(
+            dbc.FormGroup(
+                [
+                    dbc.Label(
+                        "Password", html_for="example-password-grid"
+                    ),
+                    dbc.Input(
+                        type="password",
+                        id="example-password-grid",
+                        placeholder="Enter password",
+                    ),
+                ]
+            ),
+            width=6,
+        ),
+    ],
+    form=True,
+)
+
+form = [html.H2("Using grid layout with forms"), grid]
