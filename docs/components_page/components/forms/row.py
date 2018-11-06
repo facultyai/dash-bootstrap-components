0184b48b1b6a7fafb9ff5525e722634e55c050af
tcbegley <tom@asidatascience.com>
2018-11-06 07:03:55 +0000

    Add FormGroup row example

---
+++ docs/components_page/components/forms/row.py
@@ -0,0 +1,36 @@
+import dash_bootstrap_components as dbc
+import dash_html_components as html
+
+email_input = dbc.FormGroup(
+    [
+        dbc.Label("Email", html_for="example-email-row", width=2),
+        dbc.Col(
+            dbc.Input(
+                type="email",
+                id="example-email-row",
+                placeholder="Enter email",
+            ),
+            width=10,
+        ),
+    ],
+    row=True,
+)
+
+password_input = dbc.FormGroup(
+    [
+        dbc.Label(
+            "Password", html_for="example-password-row", width=2
+        ),
+        dbc.Col(
+            dbc.Input(
+                type="password",
+                id="example-password-row",
+                placeholder="Enter password",
+            ),
+            width=10,
+        ),
+    ],
+    row=True,
+)
+
+form = [html.H2("FormGroup row"), email_input, password_input]
--- docs/components_page/forms_content.py
+++ docs/components_page/forms_content.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from .api_doc import ApiDoc
 from .components.forms.radio_check import form as form_radio_check
+from .components.forms.row import form as form_row
 from .components.forms.simple import form as form_simple
 from .helpers import ExampleContainer, HighlightedSource
 from .metadata import get_component_metadata
@@ -11,17 +12,23 @@ FORMS = HERE / "components" / "forms"
 
 form_simple_source = (FORMS / "simple.py").open().read()
 form_radio_check_source = (FORMS / "radio_check.py").open().read()
+form_row_source = (FORMS / "row.py").open().read()
 
 content = [
     ExampleContainer(form_simple),
     HighlightedSource(form_simple_source),
     ExampleContainer(form_radio_check),
     HighlightedSource(form_radio_check_source),
+    ExampleContainer(form_row),
+    HighlightedSource(form_row_source),
     ApiDoc(
         get_component_metadata("src/components/form/Form.js"),
         component_name="Form",
     ),
-    ApiDoc(get_component_metadata("src/components/form/FormGroup.js")),
+    ApiDoc(
+        get_component_metadata("src/components/form/FormGroup.js"),
+        component_name="FormGroup",
+    ),
     ApiDoc(
         get_component_metadata("src/components/input/RadioItems.js"),
         component_name="RadioItems",
