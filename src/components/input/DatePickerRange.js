9c5c37eca42a2a46e23fe10d529db096eed153e9
tcbegley <tom@asidatascience.com>
2018-11-08 12:53:53 +0000

    Add disabled_days prop to DatePickerRange

--- src/components/input/DatePickerRange.js
+++ src/components/input/DatePickerRange.js
@@ -52,6 +52,10 @@ export default class DatePickerRange extends Component {
         newState[prop].add(1, 'days');
       }
     });
+    const disabled_days = newProps['disabled_days'];
+    if (R.type(disabled_days) != 'Undefined') {
+      newState['disabled_days'] = disabled_days.map(d => moment(d));
+    }
     this.setState(newState);
   }
 
@@ -95,7 +99,7 @@ export default class DatePickerRange extends Component {
   }
 
   isOutsideRange(date) {
-    const {min_date_allowed, max_date_allowed} = this.state;
+    const {min_date_allowed, max_date_allowed, disabled_days} = this.state;
     const notUndefined = R.complement(
       R.pipe(
         R.type,
@@ -104,7 +108,8 @@ export default class DatePickerRange extends Component {
     );
     return (
       (notUndefined(min_date_allowed) && date < min_date_allowed) ||
-      (notUndefined(max_date_allowed) && date >= max_date_allowed)
+      (notUndefined(max_date_allowed) && date >= max_date_allowed) ||
+      (notUndefined(disabled_days) && disabled_days.some(d => d.isSame(date, 'day')))
     );
   }
 
@@ -348,6 +353,12 @@ DatePickerRange.propTypes = {
    * as one date is picked.
    */
   updatemode: PropTypes.oneOf(['singledate', 'bothdates']),
+
+  /**
+   * A list of days to disable in addition to any that fall outside of the range
+   * specified by min_date_allowed and max_date_allowed.
+   */
+  disabled_days: PropTypes.arrayOf(PropTypes.string),
 
   /**
    * Set the size of the DatePickerSingle
