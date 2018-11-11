56edf8043703e4ca0ecc1561b2100fcb2a2ce9f7
Pascal Bugnion <pascal@bugnion.org>
2018-11-11 14:29:16 +0000

    Add example of sizing input group

--- docs/components_page/input_group_content.py
+++ docs/components_page/input_group_content.py
@@ -5,13 +5,17 @@ from .api_doc import ApiDoc
 from .metadata import get_component_metadata
 
 from .components.input_group.simple import input_group as input_group_simple
+from .components.input_group.size import input_group as input_group_size
 
 HERE = Path(__file__).parent
 INPUT_GROUP = HERE / "components" / "input_group"
 
 input_group_simple_source = (INPUT_GROUP / "simple.py").open().read()
+input_group_size_source = (INPUT_GROUP / "size.py").open().read()
 
 content = [
     ExampleContainer(input_group_simple),
-    HighlightedSource(input_group_simple_source)
+    HighlightedSource(input_group_simple_source),
+    ExampleContainer(input_group_size),
+    HighlightedSource(input_group_size_source)
 ]
