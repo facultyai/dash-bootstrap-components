528997bbd00fca2fbd79b6965eed3505b4939c8d
Pascal Bugnion <pascal@bugnion.org>
2018-11-11 15:00:07 +0000

    Document buttons in input groups

--- components_page/input_group_content.py
+++ components_page/input_group_content.py
@@ -6,16 +6,20 @@ from .metadata import get_component_metadata
 
 from .components.input_group.simple import input_group as input_group_simple
 from .components.input_group.size import input_group as input_group_size
+from .components.input_group.button import input_group as input_group_button
 
 HERE = Path(__file__).parent
 INPUT_GROUP = HERE / "components" / "input_group"
 
 input_group_simple_source = (INPUT_GROUP / "simple.py").open().read()
 input_group_size_source = (INPUT_GROUP / "size.py").open().read()
+input_group_button_source = (INPUT_GROUP / "button.py").open().read()
 
 content = [
     ExampleContainer(input_group_simple),
     HighlightedSource(input_group_simple_source),
     ExampleContainer(input_group_size),
-    HighlightedSource(input_group_size_source)
+    HighlightedSource(input_group_size_source),
+    ExampleContainer(input_group_button),
+    HighlightedSource(input_group_button_source)
 ]
