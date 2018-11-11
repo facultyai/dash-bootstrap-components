583a7c689c57251a138db7e50ccadd5968d894ff
Pascal Bugnion <pascal@bugnion.org>
2018-11-11 10:52:45 +0000

    Example of input group with both append and prepend

--- docs/components_page/components/input_group/simple.py
+++ docs/components_page/components/input_group/simple.py
@@ -18,8 +18,15 @@ input_group = html.Div(
                         dbc.Input(placeholder="username"),
                         dbc.InputGroupAddon("@example.com", addon_type="append"),
                     ]
+                ),
+                html.Br(),
+                dbc.InputGroup(
+                    [
+                        dbc.InputGroupAddon("$", addon_type="prepend"),
+                        dbc.Input(placeholder="Amount", type="number"),
+                        dbc.InputGroupAddon(".00", addon_type="append")
+                    ]
                 )
-
             ]
         )
     ]
