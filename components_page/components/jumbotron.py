8d783f9becdd1f41a580cb8e067e6a87fdd55fbd
Pascal Bugnion <pascal@bugnion.org>
2018-11-12 15:27:42 +0000

    Additional examples of typography in jumbotron

--- components_page/components/jumbotron.py
+++ components_page/components/jumbotron.py
@@ -1,14 +1,15 @@
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 
-jumbotron = html.Div(
+jumbotron = dbc.Jumbotron(
     [
-        html.H2("Jumbotron"),
-        dbc.Jumbotron(
-            [
-                html.H1("Hello world", className="display-3"),
-                html.P("Use a jumbotron to call attention to featured content or information.", className="lead"),
-            ]
+        html.H1("Jumbotron", className="display-3"),
+        html.P("Use a jumbotron to call attention to featured content or information.", className="lead"),
+        html.Hr(className="my-2"),
+        html.P("Jumbotrons use utility classes for typography and spacing to suit the larger container."),
+        html.P(
+            dbc.Button("Learn more", color="primary"),
+            className="lead"
         )
     ]
 )
