73f0a734f19fa3f12987789f5634a374e19882c5
Pascal Bugnion <pascal@bugnion.org>
2018-11-12 13:53:35 +0000

    Add section on dropdown direction

---
+++ components_page/components/dropdown/direction.py
@@ -0,0 +1,21 @@
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+
+items = [
+    dbc.DropdownMenuItem("First"),
+    dbc.DropdownMenuItem(divider=True),
+    dbc.DropdownMenuItem("Second"),
+]
+
+dropdown = html.Div(
+    [
+        html.H4("Dropdown direction"),
+        dbc.Row(
+            [
+                dbc.Col(dbc.DropdownMenu(label="Dropup", children=items, direction="up")),
+                dbc.Col(dbc.DropdownMenu(label="Dropleft", children=items, direction="left")),
+                dbc.Col(dbc.DropdownMenu(label="Dropright", children=items, direction="right")),
+            ]
+        )
+    ]
+)
