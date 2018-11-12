0976e4e7e58c3ac3c63e3c71ca41c6e5ae6d6f11
Pascal Bugnion <pascal@bugnion.org>
2018-11-12 07:43:26 +0000

    Examples for dropdown sizing
    Since the size prop is not currently passed to the dropdown, these
    don't work yet.

---
+++ components_page/components/dropdown/size.py
@@ -0,0 +1,21 @@
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+
+items = [
+    dbc.DropdownMenuItem("First"),
+    dbc.DropdownMenuItem(divider=True),
+    dbc.DropdownMenuItem("Second"),
+]
+
+dropdown = html.Div(
+    [
+        html.H2("Dropdown sizing"),
+        html.Div(
+            [
+                dbc.DropdownMenu(label="lg dropdown", children=items, style={"margin-bottom": "1rem"}),
+                dbc.DropdownMenu(label="normal dropdown", children=items, style={"margin-bottom": "1rem"}),
+                dbc.DropdownMenu(label="sm dropdown", children=items),
+            ]
+        )
+    ]
+)
