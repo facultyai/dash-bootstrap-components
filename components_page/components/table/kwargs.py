8c0e94c48a70dbe421fb1bd7583be948cd78ce97
tcbegley <tom@asidatascience.com>
2018-11-13 10:35:55 +0000

    Adding keyword argument examples

---
+++ components_page/components/table/kwargs.py
@@ -0,0 +1,9 @@
+table = dbc.Table(
+    # using the same table as in the above example
+    table_header + table_body,
+    bordered=True,
+    dark=True,
+    hover=True,
+    responsive=True,
+    striped=True,
+)
