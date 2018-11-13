36f26f08e4feaa2ad9a2573d9ab5ae55aebf89b0
tcbegley <tom@asidatascience.com>
2018-11-13 11:55:28 +0000

    Allow adding globals to exec

--- docs/components_page/helpers.py
+++ docs/components_page/helpers.py
@@ -15,13 +15,18 @@ def ExampleContainer(component):
     return html.Div(component, className="example-container")
 
 
-def load_source_with_app(app, source, component_name):
+def load_source_with_app(
+    app, source, component_name, globals_dict=None, locals_dict=None
+):
     """
     Execute a source snippet, injecting the `app` local variable.
 
     Return the local variable defined by `component_name`. This should
     be used for source files that need to register `@app` callbacks.
     """
+    globals_dict = globals_dict if isinstance(globals_dict, dict) else {}
     exec_namespace = {"app": app}
-    exec(source, {}, exec_namespace)
+    if isinstance(locals_dict, dict):
+        exec_namespace.update(locals_dict)
+    exec(source, globals_dict, exec_namespace)
     return exec_namespace[component_name]
