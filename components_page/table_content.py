3e69444caac19ff0b9053c1ae301d6de722ce082
tcbegley <tom@asidatascience.com>
2018-11-13 10:17:28 +0000

    Create subdirectory for table

--- components_page/table_content.py
+++ components_page/table_content.py
@@ -1,17 +1,18 @@
 from pathlib import Path
 
 from .api_doc import ApiDoc
-from .components.table import table
+from .components.table.simple import table as table_simple
 from .helpers import ExampleContainer, HighlightedSource
 from .metadata import get_component_metadata
 
 HERE = Path(__file__).parent
+TABLE = HERE / "components" / "table"
 
-table_source = (HERE / "components" / "table.py").open().read()
+table_simple_source = (TABLE / "simple.py").open().read()
 
 
 content = [
-    ExampleContainer(table),
-    HighlightedSource(table_source),
+    ExampleContainer(table_simple),
+    HighlightedSource(table_simple_source),
     ApiDoc(get_component_metadata("src/components/Table.js")),
 ]
