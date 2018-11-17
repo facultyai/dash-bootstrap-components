ea9e2384d363584e9c47ac88ba4008234c20151d
Pascal Bugnion <pascal@bugnion.org>
2018-11-17 10:01:46 +0000

    Example of introducing default behaviour with button

--- docs/components_page/components/input_group/button.py
+++ docs/components_page/components/input_group/button.py
@@ -1,5 +1,7 @@
+
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
 
 input_group = html.Div(
     [
@@ -9,12 +11,25 @@ input_group = html.Div(
                 dbc.InputGroup(
                     [
                         dbc.InputGroupAddon(
-                            dbc.Button("Button text"), addon_type="prepend"
+                            dbc.Button("Random name", id="input-group-button"),
+                            addon_type="prepend"
                         ),
-                        dbc.Input(),
+                        dbc.Input(id="input-group-button-input", placeholder="name"),
                     ]
                 )
             ]
         ),
     ]
 )
+
+@app.callback(
+    Output("input-group-button-input", "value"),
+    [Input("input-group-button", "n_clicks")]
+)
+def on_button_click(n_clicks):
+    if n_clicks:
+        names = ["Arthur Dent", "Ford Prefect", "Trillian Astra"]
+        which = n_clicks % len(names)
+        return names[which]
+    else:
+        return ""
