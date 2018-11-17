843d4630c52f13a3e56348464fbf0091ccdd3cf2
Pascal Bugnion <pascal@bugnion.org>
2018-11-17 10:40:37 +0000

    Sort imports

--- components_page/components/input_group/button.py
+++ components_page/components/input_group/button.py
@@ -1,5 +1,5 @@
-import dash_html_components as html
 import dash_bootstrap_components as dbc
+import dash_html_components as html
 from dash.dependencies import Input, Output
 
 input_group = html.Div(
