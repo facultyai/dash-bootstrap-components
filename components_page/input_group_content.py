e4e76c24642ae351a917b6d541ce0876938e0dcd
Pascal Bugnion <pascal@bugnion.org>
2018-11-17 10:01:46 +0000

    Example of introducing default behaviour with button

--- components_page/input_group_content.py
+++ components_page/input_group_content.py
@@ -1,12 +1,11 @@
 from pathlib import Path
 
-from .helpers import ExampleContainer, HighlightedSource
+from .helpers import ExampleContainer, HighlightedSource, load_source_with_app
 from .api_doc import ApiDoc
 from .metadata import get_component_metadata
 
 from .components.input_group.simple import input_group as input_group_simple
 from .components.input_group.size import input_group as input_group_size
-from .components.input_group.button import input_group as input_group_button
 
 HERE = Path(__file__).parent
 INPUT_GROUP = HERE / "components" / "input_group"
@@ -15,23 +14,25 @@ input_group_simple_source = (INPUT_GROUP / "simple.py").open().read()
 input_group_size_source = (INPUT_GROUP / "size.py").open().read()
 input_group_button_source = (INPUT_GROUP / "button.py").open().read()
 
-content = [
-    ExampleContainer(input_group_simple),
-    HighlightedSource(input_group_simple_source),
-    ExampleContainer(input_group_size),
-    HighlightedSource(input_group_size_source),
-    ExampleContainer(input_group_button),
-    HighlightedSource(input_group_button_source),
-    ApiDoc(
-        get_component_metadata("src/components/input/InputGroup.js"),
-        component_name="InputGroup",
-    ),
-    ApiDoc(
-        get_component_metadata("src/components/input/InputGroupAddon.js"),
-        component_name="InputGroupAddon",
-    ),
-    ApiDoc(
-        get_component_metadata("src/components/input/InputGroupText.js"),
-        component_name="InputGroupText",
-    ),
-]
+
+def get_content(app):
+    return [
+        ExampleContainer(input_group_simple),
+        HighlightedSource(input_group_simple_source),
+        ExampleContainer(input_group_size),
+        HighlightedSource(input_group_size_source),
+        ExampleContainer(load_source_with_app(app, input_group_button_source, "input_group")),
+        HighlightedSource(input_group_button_source),
+        ApiDoc(
+            get_component_metadata("src/components/input/InputGroup.js"),
+            component_name="InputGroup",
+        ),
+        ApiDoc(
+            get_component_metadata("src/components/input/InputGroupAddon.js"),
+            component_name="InputGroupAddon",
+        ),
+        ApiDoc(
+            get_component_metadata("src/components/input/InputGroupText.js"),
+            component_name="InputGroupText",
+        ),
+    ]
