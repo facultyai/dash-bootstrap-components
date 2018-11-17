989d1323b2627229f88b6374b7a1c05f3c4bd4ee
Pascal Bugnion <pascal@bugnion.org>
2018-11-17 14:56:44 +0000

    Avoid long lines

--- docs/components_page/components/dropdown/simple.py
+++ docs/components_page/components/dropdown/simple.py
@@ -1,6 +1,8 @@
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 
+external_url = "https://github.com/asidatascience/dash-bootstrap-components"
+
 dropdown = html.Div(
     [
         html.H2("Dropdown menu"),
@@ -13,10 +15,7 @@ dropdown = html.Div(
                 dbc.DropdownMenuItem(
                     "Internal link", href="/l/components/alerts"
                 ),
-                dbc.DropdownMenuItem(
-                    "External link",
-                    href="https://github.com/asidatascience/dash-bootstrap-components",
-                ),
+                dbc.DropdownMenuItem("External link", href=external_url),
                 dbc.DropdownMenuItem(divider=True),
                 dbc.DropdownMenuItem("Disabled", disabled=True),
                 dbc.DropdownMenuItem("Active", active=True),
