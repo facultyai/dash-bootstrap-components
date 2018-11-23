0ed0dee495f9e9d0c053356b2e5a705a0912ae5a
Pascal Bugnion <pascal@bugnion.org>
2018-11-23 10:48:25 +0000

    Migrate jumbotron component doc to separate directory

--- docs/components_page/jumbotron_content.py
+++ docs/components_page/jumbotron_content.py
@@ -3,14 +3,15 @@ from pathlib import Path
 from .helpers import ExampleContainer, HighlightedSource
 from .api_doc import ApiDoc
 from .metadata import get_component_metadata
-from .components.jumbotron import jumbotron
+from .components.jumbotron.simple import jumbotron as jumbotron_simple
 
 HERE = Path(__file__).parent
+JUMBOTRON = HERE / "components" / "jumbotron"
 
-jumbotron_source = (HERE / "components" / "jumbotron.py").open().read()
+jumbotron_simple_source = (JUMBOTRON / "simple.py").open().read()
 
 content = [
-    ExampleContainer(jumbotron),
-    HighlightedSource(jumbotron_source),
+    ExampleContainer(jumbotron_simple),
+    HighlightedSource(jumbotron_simple_source),
     ApiDoc(get_component_metadata("src/components/Jumbotron.js"))
 ]
