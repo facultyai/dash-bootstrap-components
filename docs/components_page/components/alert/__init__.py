84d65d7e93b05b5daf2134d81aa2e497117781e9
tcbegley <tom@asidatascience.com>
2018-11-26 14:28:22 +0000

    Move *_content.py files

--- docs/components_page/components/alert/__init__.py
+++ docs/components_page/components/alert/__init__.py
@@ -3,17 +3,16 @@ from pathlib import Path
 import dash_html_components as html
 
 from .api_doc import ApiDoc
-from .components.alerts import alerts
+from .components.alert import alerts
 from .helpers import ExampleContainer, HighlightedSource
 from .metadata import get_component_metadata
 
 HERE = Path(__file__).parent
 
-source = (HERE / "components" / "alerts.py").read_text()
-
+source = (HERE / "components" / "alert.py").read_text()
 
 content = [
-    html.H2("Alerts"),
+    html.H2("Alert"),
     ExampleContainer(alerts),
     HighlightedSource(source),
     ApiDoc(get_component_metadata("src/components/Alert.js")),
