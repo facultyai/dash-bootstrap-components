0a193b62ab0736fac79a6c3e4223f546234b7689
tcbegley <tom@asidatascience.com>
2018-12-04 12:32:33 +0000

    Ignore unused themes import in __init__.py

--- dash_bootstrap_components/__init__.py
+++ dash_bootstrap_components/__init__.py
@@ -3,7 +3,7 @@ import sys
 
 import dash
 
-from . import themes
+from . import themes  # noqa
 from ._version import __version__  # noqa
 
 _current_path = os.path.dirname(os.path.abspath(__file__))
