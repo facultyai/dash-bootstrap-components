5e0bbedcbd946750f760cd0259e9f2a59d42a6ef
Pascal Bugnion <pascal@bugnion.org>
2018-12-09 11:07:40 +0000

    Build components into dbc.components

--- dash_bootstrap_components/__init__.py
+++ dash_bootstrap_components/__init__.py
@@ -5,16 +5,11 @@ import dash
 
 from . import themes  # noqa
 from ._version import __version__  # noqa
-
-_current_path = os.path.dirname(os.path.abspath(__file__))
-
-METADATA_PATH = os.path.join(_current_path, "metadata.json")
-
-# Dash automatic class creation from `metadata.json` file.
+from . import components
 
 _js_dist = [
     {
-        "relative_package_path": "bundle.js",
+        "relative_package_path": "dash_bootstrap_components.min.js",
         "namespace": "dash_bootstrap_components",
     }
 ]
@@ -22,14 +17,10 @@ _js_dist = [
 _css_dist = []
 
 
-def _setup_js_components(module, path_to_metadata):
-    components = dash.development.component_loader.load_components(
-        path_to_metadata, "dash_bootstrap_components"
-    )
-    for component in components:
-        setattr(module, component.__name__, component)
-        component._js_dist = _js_dist
-        component._css_dist = _css_dist
+for _component_name in components.__all__:
+    _component = getattr(components, _component_name)
+    _component._js_dist = _js_dist
+    _component._css_dist = _css_dist
 
 
-_setup_js_components(sys.modules[__name__], METADATA_PATH)
+from .components import *  # noqa
