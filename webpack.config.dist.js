f211c727a3943b7c38358ee5f58d9f21a9201493
Pascal Bugnion <pascal@bugnion.org>
2018-12-09 12:07:44 +0000

    Consistent indentation of loaders

--- webpack.config.dist.js
+++ webpack.config.dist.js
@@ -49,7 +49,14 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [{loader: 'style-loader'}, {loader: 'css-loader'}]
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
       }
     ]
   },
