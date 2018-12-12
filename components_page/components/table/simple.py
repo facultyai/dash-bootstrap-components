6ec6669a29c2c08d7f1a8fa6ed3657f5acc1aa2f
tcbegley <tom@asidatascience.com>
2018-12-12 23:47:58 +0000

    Don't use dataframe without from_dataframe method

--- components_page/components/table/simple.py
+++ components_page/components/table/simple.py
@@ -1,25 +1,15 @@
 import dash_bootstrap_components as dbc
 import dash_html_components as html
-import pandas as pd
-
-df = pd.DataFrame(
-    {
-        "First Name": ["Arthur", "Ford", "Zaphod", "Trillian"],
-        "Last Name": ["Dent", "Prefect", "Beeblebrox", "Astra"],
-    }
-)
 
 table_header = [
-    html.Thead(html.Tr(children=[html.Th(col) for col in df.columns]))
+    html.Thead(html.Tr([html.Th("First Name"), html.Th("Last Name")]))
 ]
 
-table_body = [
-    html.Tbody(
-        [
-            html.Tr([html.Td(df.loc[i, col]) for col in df.columns])
-            for i in df.index
-        ]
-    )
-]
+row1 = html.Tr([html.Td("Arthur"), html.Td("Dent")])
+row2 = html.Tr([html.Td("Ford"), html.Td("Prefect")])
+row3 = html.Tr([html.Td("Zaphod"), html.Td("Beeblebrox")])
+row4 = html.Tr([html.Td("Trillian"), html.Td("Astra")])
+
+table_body = [html.Tbody([row1, row2, row3, row4])]
 
 table = dbc.Table(table_header + table_body, bordered=True)
