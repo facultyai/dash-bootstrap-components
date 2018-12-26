c33e38bee5f0e5735ce80f671624ec7cca3f0034
tcbegley <tom@asidatascience.com>
2018-12-26 10:22:24 +0000

    Add requirements and docstrings

--- examples/gallery/iris-kmeans/app.py
+++ examples/gallery/iris-kmeans/app.py
@@ -1,3 +1,8 @@
+"""
+Dash port of Shiny iris k-means example:
+
+https://shiny.rstudio.com/gallery/kmeans-example.html
+"""
 import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
--- examples/gallery/telephones-by-region/app.py
+++ examples/gallery/telephones-by-region/app.py
@@ -1,3 +1,8 @@
+"""
+Dash port of Shiny telephones by region example:
+
+https://shiny.rstudio.com/gallery/telephones-by-region.html
+"""
 import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
