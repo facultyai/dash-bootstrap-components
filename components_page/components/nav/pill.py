cdaf1c13c8680507a2b32f3b687e34c2b6ef0385
tcbegley <tom@asidatascience.com>
2019-01-15 06:58:45 +0000

    Add documentation for Nav

---
+++ components_page/components/nav/pill.py
@@ -0,0 +1,11 @@
+import dash_bootstrap_components as dbc
+
+nav = dbc.Nav(
+    [
+        dbc.NavItem(dbc.NavLink("Active", active=True, href="#")),
+        dbc.NavItem(dbc.NavLink("A link", href="#")),
+        dbc.NavItem(dbc.NavLink("Another link", href="#")),
+        dbc.NavItem(dbc.NavLink("Disabled", disabled=True, href="#")),
+    ],
+    pills=True,
+)
