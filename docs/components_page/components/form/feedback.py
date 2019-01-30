e23346f810b9f5c8c50018d3956c0150c509b37c
tcbegley <tom@asidatascience.com>
2019-01-30 13:30:32 +0000

    Simplify FormFeedback example

--- docs/components_page/components/form/feedback.py
+++ docs/components_page/components/form/feedback.py
@@ -9,7 +9,13 @@ email_input = html.Div(
                 dbc.Label("Email"),
                 dbc.Input(id="email-input", type="email", value=""),
                 dbc.FormText("We only accept gmail..."),
-                dbc.FormFeedback(id="email-feedback"),
+                dbc.FormFeedback(
+                    "That looks like a gmail address :-)", valid=True
+                ),
+                dbc.FormFeedback(
+                    "Sorry, we only accept gmail for some reason...",
+                    valid=False,
+                ),
             ]
         )
     ]
@@ -17,25 +23,13 @@ email_input = html.Div(
 
 
 # --- Callbacks --- #
-
-# use the same function to set the `valid` prop of both input and feedback
+@app.callback(Output("email-input", "valid"), [Input("email-input", "value")])
 def check_valid(text):
     if text and text.endswith("@gmail.com"):
         return True
     return False
 
 
-app.callback(
-    Output("email-feedback", "valid"), [Input("email-input", "value")]
-)(check_valid)
-
-app.callback(Output("email-input", "valid"), [Input("email-input", "value")])(
-    check_valid
-)
-
-
-# this callback sets the input style to invalid if something has been typed
-# but it's not right.
 @app.callback(
     Output("email-input", "invalid"), [Input("email-input", "value")]
 )
@@ -43,14 +37,3 @@ def check_invalid(text):
     if text and not text.endswith("@gmail.com"):
         return True
     return False
-
-
-# this callback modifies the feedback text according to whether the input is
-# valid or invalid
-@app.callback(
-    Output("email-feedback", "children"), [Input("email-feedback", "valid")]
-)
-def make_feedback(valid):
-    if valid:
-        return "That looks like a gmail address :-)"
-    return "Sorry, we only accept gmail for some reason..."
