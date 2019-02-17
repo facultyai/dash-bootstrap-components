d24502b7179700ee870faf663c1e45f9f6e40f73
Pascal Bugnion <pascal@bugnion.org>
2019-02-17 08:07:00 +0000

    Add callback example for multi checkbox / radio group

--- docs/components_page/components/input/radio_check.py
+++ docs/components_page/components/input/radio_check.py
@@ -1,4 +1,6 @@
 import dash_bootstrap_components as dbc
+import dash_html_components as html
+from dash.dependencies import Input, Output
 
 radioitems = dbc.FormGroup(
     [
@@ -9,6 +11,7 @@ radioitems = dbc.FormGroup(
                 {"label": "Option 2", "value": 2},
             ],
             value=1,
+            id="radioitems-input"
         ),
     ]
 )
@@ -22,36 +25,18 @@ checklist = dbc.FormGroup(
                 {"label": "Option 2", "value": 2},
             ],
             values=[],
+            id="checklist-input"
         ),
     ]
 )
 
-inline_radioitems = dbc.FormGroup(
-    [
-        dbc.Label("Choose one"),
-        dbc.RadioItems(
-            options=[
-                {"label": "Option 1", "value": 1},
-                {"label": "Option 2", "value": 2},
-            ],
-            value=1,
-            inline=True,
-        ),
-    ]
-)
+inputs = html.Div([dbc.Form([radioitems, checklist]), html.P(id="radioitems-checklist-output")])
 
-inline_checklist = dbc.FormGroup(
-    [
-        dbc.Label("Choose a bunch"),
-        dbc.Checklist(
-            options=[
-                {"label": "Option 1", "value": 1},
-                {"label": "Option 2", "value": 2},
-            ],
-            values=[],
-            inline=True,
-        ),
-    ]
+@app.callback(
+    Output("radioitems-checklist-output", "children"),
+    [Input("radioitems-input", "value"), Input("checklist-input", "values")]
 )
-
-inputs = dbc.Form([radioitems, checklist, inline_radioitems, inline_checklist])
+def on_form_change(radio_items_value, checklist_values):
+    output_string = "Radio button {} and {} checklist items are selected.".format(
+        radio_items_value, len(checklist_values))
+    return output_string
