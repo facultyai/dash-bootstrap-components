dd5b4c3575b17e27bfeba4d2fe015ed2685cf3bb
Pascal Bugnion <pascal@bugnion.org>
2019-03-16 10:32:10 +0000

    Test behaviour of onKeyPress

--- src/components/input/__tests__/Input.test.js
+++ src/components/input/__tests__/Input.test.js
@@ -108,4 +108,50 @@ describe('Input', () => {
       expect(value).toEqual('some-value')
     })
   })
+
+  describe('onKeyPress for input', () => {
+    const event = { key: 'Enter' }
+
+    it('dispatch setProps', () => {
+      const setProps = jest.fn()
+      const input = shallow(
+        <Input
+          n_submit={0}
+          n_submit_timestamp={-1}
+          setProps={setProps}
+        />
+      )
+      input.simulate('keyPress', event)
+      expect(setProps.mock.calls).toHaveLength(1)
+      const [[{n_submit, n_submit_timestamp}]] = setProps.mock.calls
+      expect(n_submit).toEqual(1)
+      expect(n_submit_timestamp).toBeValidDate()
+    })
+
+    it('dispatch the value if debounce is true', () => {
+      const setProps = jest.fn()
+      const input = shallow(
+        <Input
+          n_submit={0}
+          n_submit_timestamp={-1}
+          setProps={setProps}
+          debounce={true}
+          value="some-value"
+        />
+      )
+      input.simulate('keyPress', event)
+      expect(setProps.mock.calls).toHaveLength(1)
+      const [[{n_submit, n_submit_timestamp, value}]] = setProps.mock.calls
+      expect(n_submit).toEqual(1)
+      expect(n_submit_timestamp).toBeValidDate()
+      expect(value).toEqual("some-value")
+    })
+
+    it('do nothing if the key is not enter', () => {
+      const setProps = jest.fn()
+      const input = shallow(<Input setProps={setProps} />)
+      input.simulate('keyPress', {key: 'a'})
+      expect(setProps.mock.calls).toHaveLength(0)
+    })
+  })
 })
