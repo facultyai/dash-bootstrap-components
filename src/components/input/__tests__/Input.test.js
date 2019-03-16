d5ef7418fc4fd926e6b0186e38ead3a6a4e5538c
Pascal Bugnion <pascal@bugnion.org>
2019-03-16 09:38:09 +0000

    Test setting the value

--- src/components/input/__tests__/Input.test.js
+++ src/components/input/__tests__/Input.test.js
@@ -10,4 +10,18 @@ describe('Input', () => {
     const htmlInput = input.find('input')
     expect(htmlInput).toHaveLength(1)
   })
+
+  describe('value', () => {
+    it('push no value by default', () => {
+      const input = shallow(<Input />)
+      const htmlInput = input.find('input')
+      expect(htmlInput.prop('value')).toBeUndefined()
+    })
+
+    it('pass a value onto the html input', () => {
+      const input = shallow(<Input value="some-value" />)
+      const htmlInput = input.find('input')
+      expect(htmlInput.prop('value')).toEqual('some-value')
+    })
+  })
 })
