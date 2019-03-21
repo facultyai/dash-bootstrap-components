ac46c1b88d8347f9abcb00272ee6ffa9b26ccb81
tcbegley <tom@asidatascience.com>
2019-03-21 13:41:24 +0000

    Document custom styling of tabs

--- docs/components_page/components/tabs/__init__.py
+++ docs/components_page/components/tabs/__init__.py
@@ -12,12 +12,14 @@ from ...helpers import (
 from ...metadata import get_component_metadata
 from .simple import tab1_content, tab2_content
 from .simple import tabs as tabs_simple
+from .style import tabs as tabs_style
 
 HERE = Path(__file__).parent
 
 tabs_simple_source = (HERE / "simple.py").read_text()
 tabs_active_source = (HERE / "active_tab.py").read_text()
 tabs_card_source = (HERE / "card.py").read_text()
+tabs_style_source = (HERE / "style.py").read_text()
 
 
 def get_content(app):
@@ -77,6 +79,27 @@ def get_content(app):
             )
         ),
         HighlightedSource(tabs_card_source),
+        html.H4("Styling tabs"),
+        html.P(
+            dcc.Markdown(
+                "You can modify the style of the tabs and labels using the "
+                "`tab_style` and `label_style` properties. Use `tab_style` to "
+                "modify the tab itself, for example to modify placement and "
+                "spacing. Use `label_style` to modify the label, for example "
+                "the font, text color, border-radius and so on."
+            )
+        ),
+        html.P(
+            dcc.Markdown(
+                "You can also apply CSS classes to the tab or label using the "
+                "`tabClassName` or `labelClassName` properties. In the below "
+                "example we apply the Bootstrap classes `ml-auto`, which sets "
+                "the left margin to `auto`, and `text-success`, which sets "
+                "the text color to the theme's 'success' color."
+            )
+        ),
+        ExampleContainer(tabs_style),
+        HighlightedSource(tabs_style_source),
         ApiDoc(
             get_component_metadata("src/components/tabs/Tabs.js"),
             component_name="Tabs",
