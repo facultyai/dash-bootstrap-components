be1612766b0401cdaa54416f1507c241b97f2dd0
tcbegley <tom@asidatascience.com>
2019-04-19 19:22:55 +0100

    Add sidebar with submenus example

---
+++ examples/multi-page-apps/sidebar-with-submenus/sidebar.py
@@ -0,0 +1,156 @@
+"""
+This app creates a simple sidebar layout using inline style arguments and the
+dbc.Nav component.
+
+dcc.Location is used to track the current location. There are two callbacks,
+one uses the current location to render the appropriate page content, the other
+uses the current location to toggle the "active" properties of the navigation
+links.
+
+For more details on building multi-page Dash applications, check out the Dash
+documentation: https://dash.plot.ly/urls
+"""
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+
+# link fontawesome to get the chevron icons
+FA = "https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP, FA])
+
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+submenu_1 = [
+    html.Li(
+        # use Row and Col components to position the chevrons
+        dbc.Row(
+            [
+                dbc.Col("Menu 1"),
+                dbc.Col(
+                    html.I(className="fas fa-chevron-right mr-3"), width="auto"
+                ),
+            ],
+            className="my-1",
+        ),
+        id="submenu-1",
+    ),
+    # we use the Collapse component to hide and reveal the navigation links
+    dbc.Collapse(
+        [
+            dbc.NavLink("Page 1.1", href="/page-1/1"),
+            dbc.NavLink("Page 1.2", href="/page-1/2"),
+        ],
+        id="submenu-1-collapse",
+    ),
+]
+
+submenu_2 = [
+    html.Li(
+        dbc.Row(
+            [
+                dbc.Col("Menu 2"),
+                dbc.Col(
+                    html.I(className="fas fa-chevron-right mr-3"), width="auto"
+                ),
+            ],
+            className="my-1",
+        ),
+        id="submenu-2",
+    ),
+    dbc.Collapse(
+        [
+            dbc.NavLink("Page 2.1", href="/page-2/1"),
+            dbc.NavLink("Page 2.2", href="/page-2/2"),
+        ],
+        id="submenu-2-collapse",
+    ),
+]
+
+
+sidebar = html.Div(
+    [
+        html.H2("Sidebar", className="display-4"),
+        html.Hr(),
+        html.P(
+            "A sidebar with collapsible navigation links", className="lead"
+        ),
+        dbc.Nav(submenu_1 + submenu_2, vertical=True),
+    ],
+    style=SIDEBAR_STYLE,
+    id="sidebar",
+)
+
+content = html.Div(id="page-content", style=CONTENT_STYLE)
+
+app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
+
+
+# this function is used to toggle the is_open property of each Collapse
+def toggle_collapse(n, is_open):
+    if n:
+        return not is_open
+    return is_open
+
+
+# this function applies the "open" class to rotate the chevron
+def set_navitem_class(is_open):
+    if is_open:
+        return "open"
+    return ""
+
+
+for i in [1, 2]:
+    app.callback(
+        Output(f"submenu-{i}-collapse", "is_open"),
+        [Input(f"submenu-{i}", "n_clicks")],
+        [State(f"submenu-{i}-collapse", "is_open")],
+    )(toggle_collapse)
+
+    app.callback(
+        Output(f"submenu-{i}", "className"),
+        [Input(f"submenu-{i}-collapse", "is_open")],
+    )(set_navitem_class)
+
+
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname in ["/", "/page-1/1"]:
+        return html.P("This is the content of page 1.1!")
+    elif pathname == "/page-1/2":
+        return html.P("This is the content of page 1.2. Yay!")
+    elif pathname == "/page-2/1":
+        return html.P("Oh cool, this is page 2.1!")
+    elif pathname == "/page-2/2":
+        return html.P("No way! This is page 2.2!")
+    # If the user tries to reach a different page, return a 404 message
+    return dbc.Jumbotron(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ]
+    )
+
+
+if __name__ == "__main__":
+    app.run_server(port=8888, debug=True)
