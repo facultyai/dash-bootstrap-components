be741ec2318958b2e661e0490fc458af94693d59
tcbegley <tom@asidatascience.com>
2019-04-19 13:26:44 +0100

    Add navbar example

---
+++ examples/multi-page-apps/README.md
@@ -0,0 +1,5 @@
+# Multi-page apps
+
+This directory contains some multi-page example apps, demonstrating how the
+navigation components in *dash-bootstrap-components* can be used to create easy
+to navigate dashboards.
---
+++ examples/multi-page-apps/navbar.py
@@ -0,0 +1,71 @@
+"""
+This app uses NavbarSimple to navigate between three different pages.
+
+dcc.Location is used to track the current location. There are two callbacks,
+one uses the current location to render the appropriate page content, the other
+uses the current location to toggle the "active" properties of the navigation
+links. This means the link corresponding to the current page appears active,
+indicating to the user which page they are looking at.
+
+For more details on building multi-page Dash applications, check out the Dash
+documentation: https://dash.plot.ly/urls
+"""
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+app.layout = html.Div(
+    [
+        dcc.Location(id="url"),
+        dbc.NavbarSimple(
+            children=[
+                dbc.NavLink("Page 1", href="/page-1", id="page-1-link"),
+                dbc.NavLink("Page 2", href="/page-2", id="page-2-link"),
+                dbc.NavLink("Page 3", href="/page-3", id="page-3-link"),
+            ],
+            brand="Navbar with active links",
+            color="primary",
+            dark=True,
+        ),
+        dbc.Container(id="page-content", className="pt-4"),
+    ]
+)
+
+
+# this callback uses the current pathname to set the active state of the
+# corresponding nav link to true, allowing users to tell see page they are on
+@app.callback(
+    [Output(f"page-{i}-link", "active") for i in range(1, 4)],
+    [Input("url", "pathname")],
+)
+def toggle_active_links(pathname):
+    if pathname == "/":
+        # Treat page 1 as the homepage / index
+        return True, False, False
+    return [pathname == f"/page-{i}" for i in range(1, 4)]
+
+
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname in ["/", "/page-1"]:
+        return html.P("This is the content of page 1!")
+    elif pathname == "/page-2":
+        return html.P("This is the content of page 2. Yay!")
+    elif pathname == "/page-3":
+        return html.P("Oh cool, this is page 3!")
+    # If the user tries to reach a different page, return a 404 message
+    return dbc.Jumbotron(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ]
+    )
+
+
+if __name__ == "__main__":
+    app.run_server(port=8888)
