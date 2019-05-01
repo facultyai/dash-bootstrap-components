330d85eb077edc48cf9b3526f4e3c2c22b52f0d9
tcbegley <tom@asidatascience.com>
2019-05-01 21:58:05 +0100

    Add Dropup to docs

--- docs/components_page/components/dropdown/direction.py
+++ docs/components_page/components/dropdown/direction.py
@@ -11,17 +11,25 @@ dropdown = dbc.Row(
         dbc.Col(
             dbc.DropdownMenu(
                 label="Dropdown (default)", children=items, direction="down"
-            )
+            ),
+            width="auto",
         ),
         dbc.Col(
             dbc.DropdownMenu(
                 label="Dropleft", children=items, direction="left"
-            )
+            ),
+            width="auto",
         ),
         dbc.Col(
             dbc.DropdownMenu(
                 label="Dropright", children=items, direction="right"
-            )
+            ),
+            width="auto",
         ),
-    ]
+        dbc.Col(
+            dbc.DropdownMenu(label="Dropup", children=items, direction="up"),
+            width="auto",
+        ),
+    ],
+    justify="between",
 )
