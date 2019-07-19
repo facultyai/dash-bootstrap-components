4b228c73aa4ae042510b217e352375c13b377a93
tcbegley <tomcbegley@gmail.com>
2019-07-19 12:08:47 +0100

    Updated documentation for inputs

--- components_page/components/input/radio_check.py
+++ components_page/components/input/radio_check.py
@@ -9,6 +9,7 @@ radioitems = dbc.FormGroup(
             options=[
                 {"label": "Option 1", "value": 1},
                 {"label": "Option 2", "value": 2},
+                {"label": "Disabled option", "value": 3, "disabled": True},
             ],
             value=1,
             id="radioitems-input",
@@ -23,6 +24,7 @@ checklist = dbc.FormGroup(
             options=[
                 {"label": "Option 1", "value": 1},
                 {"label": "Option 2", "value": 2},
+                {"label": "Disabled Option", "value": 3, "disabled": True},
             ],
             value=[],
             id="checklist-input",
@@ -30,9 +32,25 @@ checklist = dbc.FormGroup(
     ]
 )
 
+switches = dbc.FormGroup(
+    [
+        dbc.Label("Toggle a bunch"),
+        dbc.Checklist(
+            options=[
+                {"label": "Option 1", "value": 1},
+                {"label": "Option 2", "value": 2},
+                {"label": "Disabled Option", "value": 3, "disabled": True},
+            ],
+            value=[],
+            id="switches-input",
+            switch=True,
+        ),
+    ]
+)
+
 inputs = html.Div(
     [
-        dbc.Form([radioitems, checklist]),
+        dbc.Form([radioitems, checklist, switches]),
         html.P(id="radioitems-checklist-output"),
     ]
 )
@@ -40,9 +58,23 @@ inputs = html.Div(
 
 @app.callback(
     Output("radioitems-checklist-output", "children"),
-    [Input("radioitems-input", "value"), Input("checklist-input", "value")],
+    [
+        Input("radioitems-input", "value"),
+        Input("checklist-input", "value"),
+        Input("switches-input", "value"),
+    ],
 )
-def on_form_change(radio_items_value, checklist_value):
-    template = "Radio button {} and {} checklist items are selected."
-    output_string = template.format(radio_items_value, len(checklist_value))
+def on_form_change(radio_items_value, checklist_value, switches_value):
+    template = "Radio button {}, {} checklist item{} and {} switch{} selected."
+
+    n_checkboxes = len(checklist_value)
+    n_switches = len(switches_value)
+
+    output_string = template.format(
+        radio_items_value,
+        n_checkboxes,
+        "s" if n_checkboxes != 1 else "",
+        n_switches,
+        "es" if n_switches != 1 else "",
+    )
     return output_string
--- components_page/components/input/radio_check_inline.py
+++ components_page/components/input/radio_check_inline.py
@@ -9,6 +9,7 @@ inline_radioitems = dbc.FormGroup(
                 {"label": "Option 2", "value": 2},
             ],
             value=1,
+            id="radioitems-inline-input",
             inline=True,
         ),
     ]
@@ -23,9 +24,28 @@ inline_checklist = dbc.FormGroup(
                 {"label": "Option 2", "value": 2},
             ],
             value=[],
+            id="checklist-inline-input",
             inline=True,
         ),
     ]
 )
 
-inline_inputs = dbc.Form([inline_radioitems, inline_checklist])
+inline_switches = dbc.FormGroup(
+    [
+        dbc.Label("Toggle a bunch"),
+        dbc.Checklist(
+            options=[
+                {"label": "Option 1", "value": 1},
+                {"label": "Option 2", "value": 2},
+            ],
+            value=[],
+            id="switches-inline-input",
+            inline=True,
+            switch=True,
+        ),
+    ]
+)
+
+inline_inputs = dbc.Form(
+    [inline_radioitems, inline_checklist, inline_switches]
+)
