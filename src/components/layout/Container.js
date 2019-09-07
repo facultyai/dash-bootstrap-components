adc2d21e5a4ee383d0e282036a98dfd69a0ad7e2
tcbegley <tomcbegley@gmail.com>
2019-09-07 21:19:28 +0100

    Minor bug fixes
    Remove debounce from Input class names and don't pass setProps to
    Container

--- src/components/layout/Container.js
+++ src/components/layout/Container.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Container as RSContainer} from 'reactstrap';
+import {omit} from 'ramda';
 
 const Container = props => {
   const {children, loading_state, ...otherProps} = props;
   return (
     <RSContainer
-      {...otherProps}
+      {...omit(['setProps'], otherProps)}
       data-dash-is-loading={
         (loading_state && loading_state.is_loading) || undefined
       }
