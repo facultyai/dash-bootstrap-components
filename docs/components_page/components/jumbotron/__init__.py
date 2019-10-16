287b070cb673a834065b368a4ac013cec5b562f5
tcbegley <tomcbegley@gmail.com>
2019-10-16 08:19:49 +0100

    Update jumbotron documentation

--- docs/components_page/components/jumbotron/__init__.py
+++ docs/components_page/components/jumbotron/__init__.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import dash_core_components as dcc
 import dash_html_components as html
 
 from ...api_doc import ApiDoc
@@ -14,10 +15,23 @@ jumbotron_simple_source = (HERE / "simple.py").read_text()
 jumbotron_fluid_source = (HERE / "fluid.py").read_text()
 
 content = [
-    html.H2("Jumbotron"),
+    html.H2("Jumbotron", className="display-4"),
+    html.P(
+        dcc.Markdown(
+            "Lightweight component for showcasing key content and messages."
+        ),
+        className="lead",
+    ),
+    html.H4("Simple example"),
     ExampleContainer(jumbotron_simple),
     HighlightedSource(jumbotron_simple_source),
     html.H4("Fluid jumbotron"),
+    html.P(
+        dcc.Markdown(
+            "To make the jumbotron full width, and without rounded corners, "
+            "set `fluid=True`."
+        )
+    ),
     ExampleContainer(jumbotron_fluid),
     HighlightedSource(jumbotron_fluid_source),
     ApiDoc(get_component_metadata("src/components/Jumbotron.js")),
