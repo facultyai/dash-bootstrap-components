812c3ca4d5f8bf2daf40f87d3dcd9a1e264c9d7c
tcbegley <tomcbegley@gmail.com>
2019-10-18 20:51:30 +0100

    Update Table documentation

--- docs/components_page/components/table/kwargs_source.py
+++ docs/components_page/components/table/kwargs_source.py
@@ -3,7 +3,7 @@ import dash_bootstrap_components as dbc
 from .simple import table as simple_table
 
 table = dbc.Table(
-    simple_table.children[1].children,
+    simple_table.children,
     bordered=True,
     dark=True,
     hover=True,
