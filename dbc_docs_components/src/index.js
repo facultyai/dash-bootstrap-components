9f32ece18f495110a94e5b1a7150f2904af0fc58
tcbegley <tomcbegley@gmail.com>
2020-03-03 19:01:04 +0000

    Kill Hugo

--- dbc_docs_components/src/index.js
+++
@@ -1,1 +0,0 @@
-export {default as SyntaxHighlighter} from './components/SyntaxHighlighter.react';
--- docs-frontend/gatsby-browser.js
+++
@@ -1,2 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'prismjs/themes/prism-tomorrow.css';
