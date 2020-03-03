5b719e22c001fff9d27fb525af0915b7e59bdfa5
tcbegley <tomcbegley@gmail.com>
2020-03-03 19:01:04 +0000

    Kill Hugo

--- docs/dbc_docs_components/dbc_docs_components/_imports_.py
+++
@@ -1,5 +0,0 @@
-from .SyntaxHighlighter import SyntaxHighlighter
-
-__all__ = [
-    "SyntaxHighlighter"
-]
--- docs/wsgi.py
+++ docs/wsgi.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
-from werkzeug.serving import run_simple
-
 from components_page.register_apps import register
 
 server = Flask(__name__, static_folder="assets")
@@ -20,4 +18,6 @@ application = DispatcherMiddleware(
 
 
 if __name__ == "__main__":
+    from werkzeug.serving import run_simple
+
     run_simple("localhost", 8888, application, use_reloader=True)
