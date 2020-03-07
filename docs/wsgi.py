f473d944b462283382d79859fe2d3803725adf87
tcbegley <tomcbegley@gmail.com>
2020-03-07 08:31:13 +0000

    Build out Flask app using templates

--- docs/wsgi.py
+++ docs/wsgi.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template
+from flask import Blueprint, Flask, abort, render_template
+from jinja2 import TemplateNotFound
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 from components_page.register_apps import register
 
 server = Flask(__name__)
+docs = Blueprint("docs", __name__)
 
 
 @server.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("test.html")
 
+
+@docs.route("/docs/", defaults={"page": "index"})
+@docs.route("/docs/<page>")
+def show(page):
+    try:
+        return render_template(f"docs/{page}.html")
+    except TemplateNotFound:
+        abort(404)
+
+
+server.register_blueprint(docs)
 
 routes = register()
 application = DispatcherMiddleware(
