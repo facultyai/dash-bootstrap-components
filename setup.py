4c415c2237c53fa413ae9e6a461919e1dc4e5564
tcbegley <tomcbegley@gmail.com>
2020-03-12 07:00:20 +0000

    Fix setup.py for Python 2

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import io
 import os
 
 from setuptools import find_packages, setup
@@ -19,7 +20,7 @@ def _get_version():
 
 
 def _get_long_description():
-    with open(os.path.join(HERE, "landing-page.md"), encoding="utf8") as f:
+    with io.open(os.path.join(HERE, "landing-page.md"), encoding="utf8") as f:
         return f.read()
 
 
