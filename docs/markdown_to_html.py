ebfc37892a786c1fcfe86d326a009f3b2855eb80
tcbegley <tomcbegley@gmail.com>
2020-03-14 08:30:08 +0000

    Add Changelog

--- docs/markdown_to_html.py
+++ docs/markdown_to_html.py
@@ -9,7 +9,12 @@ FencedBlockPreprocessor.LANG_TAG = ' class="language-%s"'
 CONTENT = Path(__file__).parent / "content"
 DEST = Path(__file__).parent / "templates" / "generated"
 
-HTML_TEMPLATE = """{% extends "docs.html" %}
+DOCS_HTML_TEMPLATE = """{% extends "docs.html" %}
+{% block title %}<title><TITLE></title>{% endblock %}
+{% block content %}<CONTENT>{% endblock %}
+"""
+
+CHANGELOG_HTML_TEMPLATE = """{% extends "changelog.html" %}
 {% block title %}<title><TITLE></title>{% endblock %}
 {% block content %}<CONTENT>{% endblock %}
 """
@@ -22,15 +27,17 @@ def convert_all_markdown_files():
             f.write(template)
 
     for path in CONTENT.glob("*.md"):
-        template = template_from_markdown(path)
+        template = template_from_markdown(
+            path, template=CHANGELOG_HTML_TEMPLATE
+        )
         with open(DEST / path.name.replace(".md", ".html"), "w") as f:
             f.write(template)
 
 
-def template_from_markdown(path, title_suffix=""):
+def template_from_markdown(path, title_suffix="", template=DOCS_HTML_TEMPLATE):
     md = markdown.Markdown(extensions=["fenced_code", "meta"])
     text = path.read_text()
-    template = HTML_TEMPLATE.replace("<CONTENT>", md.convert(text))
+    template = template.replace("<CONTENT>", md.convert(text))
     return template.replace("<TITLE>", f"{md.Meta['title'][0]} - dbc docs")
 
 
