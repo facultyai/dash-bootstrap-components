f4033b0a3f0072ce60d38f12a12770ac41a5b87a
tcbegley <tomcbegley@gmail.com>
2020-05-09 08:02:27 +0100

    Support persistence in Checkbox and RadioButton

--- src/components/input/Checkbox.js
+++ src/components/input/Checkbox.js
@@ -22,7 +22,17 @@ class Checkbox extends React.Component {
       <input
         type="checkbox"
         checked={checked}
-        {...omit(['checked', 'setProps', 'loading_state'], this.props)}
+        {...omit(
+          [
+            'checked',
+            'setProps',
+            'loading_state',
+            'persistence',
+            'persistence_type',
+            'persisted_props'
+          ],
+          this.props
+        )}
         onClick={() => {
           if (this.props.setProps) {
             this.props.setProps({
@@ -40,6 +50,11 @@ class Checkbox extends React.Component {
     );
   }
 }
+
+Checkbox.defaultProps = {
+  persisted_props: ['checked'],
+  persistence_type: 'local'
+};
 
 Checkbox.propTypes = {
   /**
@@ -92,7 +107,36 @@ Checkbox.propTypes = {
      * Holds the name of the component that is loading
      */
     component_name: PropTypes.string
-  })
+  }),
+
+  /**
+   * Used to allow user interactions in this component to be persisted when
+   * the component - or the page - is refreshed. If `persisted` is truthy and
+   * hasn't changed from its previous value, a `value` that the user has
+   * changed while using the app will keep that change, as long as
+   * the new `value` also matches what was given originally.
+   * Used in conjunction with `persistence_type`.
+   */
+  persistence: PropTypes.oneOfType([
+    PropTypes.bool,
+    PropTypes.string,
+    PropTypes.number
+  ]),
+
+  /**
+   * Properties whose user interactions will persist after refreshing the
+   * component or the page. Since only `value` is allowed this prop can
+   * normally be ignored.
+   */
+  persisted_props: PropTypes.arrayOf(PropTypes.oneOf(['checked'])),
+
+  /**
+   * Where persisted user changes will be stored:
+   * memory: only kept in memory, reset on page refresh.
+   * local: window.localStorage, data is kept after the browser quit.
+   * session: window.sessionStorage, data is cleared once the browser quit.
+   */
+  persistence_type: PropTypes.oneOf(['local', 'session', 'memory'])
 };
 
 export default Checkbox;
