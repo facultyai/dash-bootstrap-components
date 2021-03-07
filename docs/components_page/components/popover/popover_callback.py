331dea3c76214fb6febf9aec01729a0353cb5641
tcbegley <tomcbegley@gmail.com>
2021-03-07 09:21:05 +0000

    Add documentation

---
+++ docs/components_page/components/popover/popover_callback.py
@@ -0,0 +1,30 @@
+import dash_bootstrap_components as dbc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+
+popover = html.Div(
+    [
+        dbc.Button("Toggle", id="toggle", color="success", className="mr-4"),
+        dbc.Button("Target", id="target", color="danger"),
+        dbc.Popover(
+            [
+                dbc.PopoverHeader("Popover header"),
+                dbc.PopoverBody("And here's some amazing content. Cool!"),
+            ],
+            id="popover",
+            is_open=False,
+            target="target",
+        ),
+    ]
+)
+
+
+@app.callback(
+    Output("popover", "is_open"),
+    [Input("toggle", "n_clicks")],
+    [State("popover", "is_open")],
+)
+def toggle_popover(n, is_open):
+    if n:
+        return not is_open
+    return is_open
