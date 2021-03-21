a6131ed5d19bc5a00a7e8f94233c8191105454f7
tcbegley <tomcbegley@gmail.com>
2021-03-21 18:57:10 +0000

    R release 0.12.0

--- R/dbcPopover.R
+++ R/dbcPopover.R
@@ -1,8 +1,8 @@
 # AUTO GENERATED FILE - DO NOT EDIT
 
-dbcPopover <- function(children=NULL, id=NULL, style=NULL, className=NULL, key=NULL, placement=NULL, target=NULL, container=NULL, is_open=NULL, hide_arrow=NULL, innerClassName=NULL, delay=NULL, offset=NULL, flip=NULL, loading_state=NULL) {
+dbcPopover <- function(children=NULL, id=NULL, style=NULL, className=NULL, key=NULL, placement=NULL, target=NULL, container=NULL, trigger=NULL, is_open=NULL, hide_arrow=NULL, innerClassName=NULL, delay=NULL, offset=NULL, flip=NULL, loading_state=NULL) {
     
-    props <- list(children=children, id=id, style=style, className=className, key=key, placement=placement, target=target, container=container, is_open=is_open, hide_arrow=hide_arrow, innerClassName=innerClassName, delay=delay, offset=offset, flip=flip, loading_state=loading_state)
+    props <- list(children=children, id=id, style=style, className=className, key=key, placement=placement, target=target, container=container, trigger=trigger, is_open=is_open, hide_arrow=hide_arrow, innerClassName=innerClassName, delay=delay, offset=offset, flip=flip, loading_state=loading_state)
     if (length(props) > 0) {
         props <- props[!vapply(props, is.null, logical(1))]
     }
@@ -10,7 +10,7 @@ dbcPopover <- function(children=NULL, id=NULL, style=NULL, className=NULL, key=N
         props = props,
         type = 'Popover',
         namespace = 'dash_bootstrap_components',
-        propNames = c('children', 'id', 'style', 'className', 'key', 'placement', 'target', 'container', 'is_open', 'hide_arrow', 'innerClassName', 'delay', 'offset', 'flip', 'loading_state'),
+        propNames = c('children', 'id', 'style', 'className', 'key', 'placement', 'target', 'container', 'trigger', 'is_open', 'hide_arrow', 'innerClassName', 'delay', 'offset', 'flip', 'loading_state'),
         package = 'dashBootstrapComponents'
         )
 
