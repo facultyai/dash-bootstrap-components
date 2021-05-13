a7c45fefa55a41e1193c70f298fb392609d067fa
Ann Marie Ward <amward@fastmail.us>
2021-05-13 07:49:00 -0700

    R Alert component examples

---
+++ docs/components_page/components/alert/auto_dismiss.R
@@ -0,0 +1,34 @@
+
+
+library(dashBootstrapComponents)
+library(dashHtmlComponents)
+
+
+alert <- htmlDiv(
+ list(
+    dbcButton("Toggle", id = "alert-toggle-auto", className = "mr-1",
+              n_clicks = 0),
+    htmlHr(),
+    dbcAlert(
+      "Hello! I am an auto-dismissing alert!",
+      id = "alert-auto",
+      is_open = TRUE,
+      duration = 4000
+    )
+  )
+)
+
+
+app$callback(
+  output(id = "alert-auto", property = "is_open"),
+  list(
+    input(id = "alert-toggle-auto", property = "n_clicks"),
+    state(id = "alert-auto", property = "is_open")
+  ),
+  function(n, is_open) {
+    if (n > 0) {
+      return(!is_open)
+    }
+    return(is_open)
+  }
+)
