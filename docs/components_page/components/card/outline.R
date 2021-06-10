e27486297f09681d7a586c47e43205d3d4fa59d8
Ann Marie Ward <amward@fastmail.us>
2021-06-10 11:36:04 -0700

    R Card examples

---
+++ docs/components_page/components/card/outline.R
@@ -0,0 +1,45 @@
+library(dashBootstrapComponents)
+library(dashHtmlComponents)
+
+card_content <- list(
+  dbcCardHeader("Card Header"),
+  dbcCardBody(
+    list(
+      htmlH5("Card Title", className = "card-title"),
+      htmlP(
+        "This is some card content that we'll reuse",
+        className = "card-text"
+      )
+    )
+  )
+)
+
+row_1 <- dbcRow(
+  list(
+    dbcCol(dbcCard(card_content, color = "primary", outline = TRUE)),
+    dbcCol(dbcCard(card_content, color = "secondary", outline = TRUE)),
+    dbcCard(card_content, color = "info", outline = TRUE)
+  ),
+  className = "mb-4"
+)
+
+row_2 <- dbcRow(
+  list(
+    dbcCol(dbcCard(card_content, color = "success", outline = TRUE)),
+    dbcCol(dbcCard(card_content, color = "warning", outline = TRUE)),
+    dbcCard(card_content, color = "danger", outline = TRUE)
+  ),
+  className = "mb-4"
+)
+
+row_3 <- dbcRow(
+  list(
+    dbcCol(dbcCard(card_content, color = "light", outline = TRUE)),
+    dbcCol(dbcCard(card_content, color = "dark", outline = TRUE))
+  )
+)
+cards <- htmlDiv(
+  list(
+    row_1, row_2, row_3
+  )
+)
