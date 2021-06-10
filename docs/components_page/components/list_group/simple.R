785f727b9c8f1deace155d9221f0446aae506bef
Ann Marie Ward <amward@fastmail.us>
2021-06-10 13:55:49 -0700

    R List Group examples

---
+++ docs/components_page/components/list_group/simple.R
@@ -0,0 +1,9 @@
+library(dashBootstrapComponents)
+
+list_group_simple <- dbcListGroup(
+  list(
+    dbcListGroupItem("Item 1"),
+    dbcListGroupItem("Item 2"),
+    dbcListGroupItem("Item 3")
+  )
+)
