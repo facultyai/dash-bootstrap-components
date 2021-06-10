3bb5c25d393c860b5abe1d5b872632d2c71a201c
Ann Marie Ward <amward@fastmail.us>
2021-06-10 13:23:11 -0700

    R Fade examples

---
+++ docs/components_page/components/fade/fade.R
@@ -0,0 +1,36 @@
+library(dashBootstrapComponents)
+library(dashHtmlComponents)
+
+fade <- htmlDiv(
+  list(
+    dbcButton("Toggle fade", id = "fade-button", n_clicks = 0,
+              className = "mb-3"),
+    dbcFade(
+      dbcCard(
+        dbcCardBody(
+            htmlP(
+              "This content fades in and out", className = "card-text"
+            )
+        )
+      ),
+      id = "fade",
+      is_in = TRUE,
+      appear = FALSE,
+    )
+  )
+)
+
+
+app$callback(
+  output("fade", "is_in"),
+  list(
+    input("fade-button", "n_clicks"),
+    state("fade", "is_in")
+  ),
+  function(n, is_in) {
+    if (n > 0) {
+      return(!is_in)
+    }
+    return(TRUE)
+  }
+)
