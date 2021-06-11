779908b98dcdde38c7135c2499705a2dc69eda8d
Ann Marie Ward <amward@fastmail.us>
2021-06-11 08:00:49 -0700

    R Tooltip examples

---
+++ components_page/components/tooltip/tooltip.R
@@ -0,0 +1,23 @@
+library(dashBootstrapComponents)
+library(dashHtmlComponents)
+
+tooltip <- htmlDiv(
+  list(
+    htmlP(
+      list(
+        "I wonder what ",
+        htmlSpan(
+            "floccinaucinihilipilification",
+            id = "tooltip-target",
+            style = list(textDecoration = "underline", cursor = "pointer"),
+        ),
+        " means?"
+      )
+    ),
+    dbcTooltip(
+      "Noun: rare,
+      the action or habit of estimating something as worthless.",
+      target = "tooltip-target",
+    )
+  )
+)
