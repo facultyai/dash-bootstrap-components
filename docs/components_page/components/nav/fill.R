96ecca1b3e4a6e8d2c6fe5301d916f95aafdeefd
Ann Marie Ward <amward@fastmail.us>
2021-06-11 07:58:35 -0700

    R Nav examples

---
+++ docs/components_page/components/nav/fill.R
@@ -0,0 +1,20 @@
+library(dashBootstrapComponents)
+library(dashHtmlComponents)
+
+nav1 <- dbcNav(
+  list(
+    dbcNavItem(dbcNavLink("A link", href = "#")),
+    dbcNavItem(dbcNavLink("Another link with a longer label", href = "#"))
+  ),
+  fill = TRUE
+)
+
+nav2 <- dbcNav(
+  list(
+    dbcNavItem(dbcNavLink("A link", href = "#")),
+    dbcNavItem(dbcNavLink("Another link with a longer label", href = "#"))
+  ),
+  justified = TRUE
+)
+
+navs_fill <- htmlDiv(list(nav1, htmlHr(), nav2))
