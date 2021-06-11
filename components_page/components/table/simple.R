0a9a82c43d5be819e7a68f906416a45b1730c7e2
Ann Marie Ward <amward@fastmail.us>
2021-06-11 08:00:05 -0700

    R Table examples

---
+++ components_page/components/table/simple.R
@@ -0,0 +1,16 @@
+library(dashBootstrapComponents)
+library(dashHtmlComponents)
+
+table_header <- list(
+  htmlThead(
+    htmlTr(list(htmlTh("First Name"), htmlTh("Last Name"))))
+)
+
+row1 <- htmlTr(list(htmlTd("Arthur"), htmlTd("Dent")))
+row2 <- htmlTr(list(htmlTd("Ford"), htmlTd("Prefect")))
+row3 <- htmlTr(list(htmlTd("Zaphod"), htmlTd("Beeblebrox")))
+row4 <- htmlTr(list(htmlTd("Trillian"), htmlTd("Astra")))
+
+table_body <- list(htmlTbody(list(row1, row2, row3, row4)))
+
+table <- dbcTable(c(table_header, table_body), bordered = TRUE)
