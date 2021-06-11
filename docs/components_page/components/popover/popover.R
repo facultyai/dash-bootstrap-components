12e5d65eecca207b329db13b5e24b74798020b34
Ann Marie Ward <amward@fastmail.us>
2021-06-11 07:59:11 -0700

    R popover examples

---
+++ docs/components_page/components/popover/popover.R
@@ -0,0 +1,50 @@
+library(dashBootstrapComponents)
+library(dashHtmlComponents)
+
+popover_children <- list(
+    dbcPopoverHeader("Popover header"),
+    dbcPopoverBody("And here's some amazing content. Cool!")
+)
+
+popovers <- htmlDiv(
+  list(
+    dbcButton(
+      "Click", id = "click-target", n_clicks = 0,
+      color = "danger", className = "mr-1"
+    ),
+    dbcPopover(
+      popover_children,
+      id = "click",
+      target = "click-target",
+      trigger = "click"
+    ),
+    dbcButton(
+      "Focus", id = "focus-target", n_clicks = 0,
+      color = "danger", className = "mr-1"
+    ),
+    dbcPopover(
+      popover_children,
+      id = "focus",
+      target = "focus-target",
+      trigger = "focus"
+    ),
+    dbcButton(
+      "Hover", id = "hover-target", n_clicks = 0,
+      color = "danger", className = "mr-1"
+    ),
+    dbcPopover(
+      popover_children,
+      id = "hover",
+      target = "hover-target",
+      trigger = "hover",
+    ),
+    dbcButton("Legacy", id = "legacy-target", n_clicks = 0,
+              color = "danger"),
+    dbcPopover(
+        popover_children,
+        id = "legacy",
+        target = "legacy-target",
+        trigger = "legacy"
+    )
+  )
+)
