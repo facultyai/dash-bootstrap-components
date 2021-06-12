a2f9c47a23e1e97479cb888d1b29ada879fc4d86
glsdown <gemmalsdown@icloud.com>
2021-06-12 21:04:57 +0100

    Add tests for Julia snippets
    Based on R style of tests, added those for Julia too.

--- docs/components_page/components/__tests__/test_snippets.py
+++ docs/components_page/components/__tests__/test_snippets.py
@@ -1,6 +1,7 @@
 """
 Automated testing of non-Python code snippets in the docs
 """
+import json
 import re
 import unittest
 from collections import defaultdict
@@ -8,10 +9,10 @@ from pathlib import Path
 
 import pytest
 import requests
-from dash.testing.composite import DashRComposite
+from dash.testing.composite import DashRComposite, DashJuliaComposite
 
 from .helpers import clean_path, drop_keys, py_source_to_app
-from .wrappers import PY_WRAPPER, R_WRAPPER
+from .wrappers import PY_WRAPPER, R_WRAPPER, JL_WRAPPER
 
 HERE = Path(__file__).parent
 
@@ -50,15 +51,35 @@ def dashr(request, dashr_server, tmpdir):
         yield dc
 
 
+@pytest.fixture
+def dashjl(request, dashjl_server, tmpdir):
+    with DashJuliaComposite(
+        dashjl_server,
+        browser=request.config.getoption("webdriver"),
+        remote=request.config.getoption("remote"),
+        remote_url=request.config.getoption("remote_url"),
+        headless=request.config.getoption("headless"),
+        options=request.config.hook.pytest_setup_options(),
+        download_path=tmpdir.mkdir("download-jl").strpath,
+        percy_assets_root=request.config.getoption("percy_assets"),
+        percy_finalize=request.config.getoption("nopercyfinalize"),
+        pause=request.config.getoption("pause"),
+    ) as dc:
+        yield dc
+
+
 @pytest.mark.parametrize("config", PARAMS)
-def test_snippets(dash_duo, dashr, config):
+def test_snippets(dash_duo, dashr, dashjl, config):
     path, name = config
 
     if path not in SKIP:
         env = ENVS[path]
         path = HERE.parent / clean_path(path)
         r_snippet = path.parent / f"{path.stem}.R"
-        if r_snippet.exists():
+        julia_snippet = path.parent / f"{path.stem}.jl"
+
+        if r_snippet.exists() or julia_snippet.exists():
+
             # Get python snippet layout
             app = py_source_to_app(
                 PY_WRAPPER.format(snippet=path.read_text(), name=name), env=env
@@ -68,13 +89,32 @@ def test_snippets(dash_duo, dashr, config):
                 f"{dash_duo.server_url}/_dash-layout"
             ).json()
 
-            # Get R snippet layout
-            dashr.start_server(
-                R_WRAPPER.format(snippet=r_snippet.read_text(), name=name)
-            )
-            r_layout = requests.get(f"{dashr.server_url}/_dash-layout").json()
+            if r_snippet.exists():
+                # Get R snippet layout
+                dashr.start_server(
+                    R_WRAPPER.format(snippet=r_snippet.read_text(), name=name)
+                )
+                r_layout = requests.get(
+                    f"{dashr.server_url}/_dash-layout"
+                ).json()
 
-            # Test layouts match
-            unittest.TestCase().assertDictEqual(
-                drop_keys(py_layout), drop_keys(r_layout)
-            )
+                # Test layouts match
+                unittest.TestCase().assertDictEqual(
+                    drop_keys(py_layout), drop_keys(r_layout)
+                )
+
+            if julia_snippet.exists():
+                # Get Julia snippet layout
+                dashjl.start_server(
+                    JL_WRAPPER.format(
+                        snippet=julia_snippet.read_text(), name=name
+                    )
+                )
+                julia_layout = requests.get(
+                    f"{dashjl.server_url}/_dash-layout"
+                ).json()
+
+                # Test layouts match
+                unittest.TestCase().assertDictEqual(
+                    drop_keys(py_layout), drop_keys(julia_layout)
+                )
