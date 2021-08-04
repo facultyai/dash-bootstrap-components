fe1e9acdee1d747a474c47878d446ae0cbcffd3a
AnnMarieW <amward@fastmail.com>
2021-08-03 17:39:36 -0700

    Julia snippets for Carousel

---
+++ docs/components_page/components/carousel/callback.jl
@@ -0,0 +1,34 @@
+using DashBootstrapComponents, DashHtmlComponents
+
+carousel = html_div([
+   dbc_carousel(
+        id="carousel",
+        items=[
+            Dict("key" => "1", "src" => "/static/images/slide1.svg"),
+            Dict("key" => "2", "src" => "/static/images/slide2.svg"),
+            Dict("key" => "3", "src" => "/static/images/slide3.svg"),
+        ],
+        controls=false,
+        indicators=false,
+        interval=false,
+    ),
+
+    dbc_radioitems(
+        id="slide-number",
+        options=[
+            Dict("label" => "Slide 1", "value" => 0),
+            Dict("label" => "Slide 2", "value" => 1),
+            Dict("label" => "Slide 3", "value" => 2),
+        ],
+        value=0,
+        inline=true,
+    ),
+]);
+
+callback!(
+    app,
+    Output("carousel", "active_index"),
+    Input("slider-number", "value"),
+) do idx
+    return idx
+end;
