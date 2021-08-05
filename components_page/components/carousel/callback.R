4ee117e65e79c8f9c7aa41d2f625641f08c629b8
AnnMarieW <72614349+AnnMarieW@users.noreply.github.com>
2021-08-04 23:33:27 -0700

    R and Julia snippets for Carousel (#633)

---
+++ components_page/components/carousel/callback.R
@@ -0,0 +1,36 @@
+library(dashBootstrapComponents)
+library(dashHtmlComponents)
+
+carousel <- htmlDiv(
+  list(
+    dbcCarousel(
+      id = "carousel",
+      items = list (
+        list(key = "1", src = "/static/images/slide1.svg"),
+        list(key = "2", src = "/static/images/slide2.svg"),
+        list(key = "3", src = "/static/images/slide3.svg")
+      ),
+      controls = FALSE,
+      indicators = FALSE,
+      interval = FALSE
+    ),
+    dbcRadioItems(
+      id="slide-number",
+      options = list(
+        list(label = "Slide 1", value = 0),
+        list(label = "Slide 2", value = 1),
+        list(label = "Slide 3", value = 2)
+      ),
+      value = 0,
+      inline = TRUE
+    )
+  )
+)
+
+app$callback(
+  output("carousel", "active_index"),
+  list(input("slide-number", "value")),
+  function(idx) {
+    return(idx)
+  }
+)
