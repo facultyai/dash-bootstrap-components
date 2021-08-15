cecdbfd2440c51a4c519b526900586183cee84a3
glsdown <gemmalsdown@icloud.com>
2021-08-15 11:24:38 +0100

    ABetter handling of pagination component

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import {default as RBPagination} from 'react-bootstrap/Pagination';
 
@@ -8,9 +8,130 @@ import {default as RBPagination} from 'react-bootstrap/Pagination';
  * component.
  */
 const Pagination = props => {
-  const {children, ...otherProps} = props;
+  const {
+    step,
+    active_page,
+    min_value,
+    fully_expanded,
+    previous_next,
+    first_last,
+    setProps,
+    ...otherProps
+  } = props;
 
-  return <RBPagination {...otherProps}>{children}</RBPagination>;
+  let {max_value} = props;
+  // Check max_value is correct value re. step size and if not, change it
+  // i.e. min = 1, step = 2, max_value = 4 doesn't work, so need max_value = 5
+  if ((max_value - min_value) % step !== 0) {
+    max_value = max_value + step - ((max_value - min_value) % step);
+  }
+
+  // Functiont o set the active page
+  const setActivePage = value => {
+    if (setProps) {
+      setProps({active_page: value});
+    }
+  };
+
+  // Generator for the numbered pages
+  const getPaginationItem = i => {
+    return (
+      <RBPagination.Item
+        active={i === active_page}
+        onClick={() => setActivePage(i)}
+      >
+        {i}
+      </RBPagination.Item>
+    );
+  };
+
+  let paginationItems = [];
+
+  // Add the 'first' button if specified
+  if (first_last) {
+    paginationItems.push(
+      <RBPagination.First
+        disabled={active_page === min_value}
+        onClick={() => setActivePage(min_value)}
+      />
+    );
+  }
+
+  // Add a 'previous' button if specified
+  if (previous_next) {
+    paginationItems.push(
+      <RBPagination.Prev
+        disabled={active_page === min_value}
+        onClick={() => setActivePage(active_page - step)}
+      />
+    );
+  }
+  // Add the main number block in the middle
+  // As we have a minimum of 5 blocks i.e. 1 ... active ... max there is no
+  // point in not fully expanding it if there are less than 5 blocks required
+  if (fully_expanded || Math.floor((max_value - min_value) / step) + 1 <= 7) {
+    for (let i = min_value; i <= max_value; i = i + step) {
+      paginationItems.push(getPaginationItem(i));
+    }
+  } else {
+    // If it's not fully expanded, need to create the right blocks
+    paginationItems.push(getPaginationItem(min_value));
+    if (active_page <= min_value + step * 3) {
+      // If in the first 2 blocks, needs to be in the pattern 1, 2, 3, ..., 10
+      paginationItems.push(getPaginationItem(min_value + step));
+      paginationItems.push(getPaginationItem(min_value + step * 2));
+      paginationItems.push(getPaginationItem(min_value + step * 3));
+      paginationItems.push(getPaginationItem(min_value + step * 4));
+      paginationItems.push(<RBPagination.Ellipsis disabled={true} />);
+    } else if (active_page >= max_value - step * 3) {
+      // If in the last 2 blocks, needs to be in the pattern 1, ..., 8, 9, 10
+      paginationItems.push(<RBPagination.Ellipsis disabled={true} />);
+      paginationItems.push(getPaginationItem(max_value - step * 4));
+      paginationItems.push(getPaginationItem(max_value - step * 3));
+      paginationItems.push(getPaginationItem(max_value - step * 2));
+      paginationItems.push(getPaginationItem(max_value - step));
+    } else {
+      // Otherwise, needs to be in the pattern 1, ..., 5, ..., 10
+      paginationItems.push(<RBPagination.Ellipsis disabled={true} />);
+      paginationItems.push(getPaginationItem(active_page - step));
+      paginationItems.push(getPaginationItem(active_page));
+      paginationItems.push(getPaginationItem(active_page + step));
+      paginationItems.push(<RBPagination.Ellipsis disabled={true} />);
+    }
+    // Add the biggest number
+    paginationItems.push(getPaginationItem(max_value));
+  }
+  // Add the 'next' button if specified
+  if (previous_next) {
+    paginationItems.push(
+      <RBPagination.Next
+        disabled={active_page === max_value}
+        onClick={() => setActivePage(active_page + step)}
+      />
+    );
+  }
+
+  // Add the 'last' button if specified
+  if (first_last) {
+    paginationItems.push(
+      <RBPagination.Last
+        disabled={active_page === max_value}
+        onClick={() => setActivePage(max_value)}
+      />
+    );
+  }
+
+  // Create the pagination component
+  return <RBPagination {...otherProps}>{paginationItems}</RBPagination>;
+};
+
+Pagination.defaultProps = {
+  min_value: 1,
+  step: 1,
+  active_page: 1,
+  fully_expanded: true,
+  previous_next: false,
+  first_last: false
 };
 
 Pagination.propTypes = {
@@ -20,11 +141,6 @@ Pagination.propTypes = {
    * components in an app.
    */
   id: PropTypes.string,
-
-  /**
-   * The children of this component.
-   */
-  children: PropTypes.node,
 
   /**
    * Often used with CSS to style elements with common properties.
@@ -37,9 +153,49 @@ Pagination.propTypes = {
   style: PropTypes.object,
 
   /**
-   * Set the size of all page items in the pagination
+   * Set the size of all page items in the pagination.
    */
-  size: PropTypes.oneOf(['sm', 'lg'])
+  size: PropTypes.oneOf(['sm', 'lg']),
+
+  /**
+   * Minimum (leftmost) value to appear in the pagination.
+   */
+  min_value: PropTypes.number,
+
+  /**
+   * Maximum (rightmost) value to appear in the pagination. Must be defined.
+   * If the `min_value` and `step` together cannot reach this value, then the
+   * next stepped value is used as the maximum.
+   */
+  max_value: PropTypes.number.isRequired,
+
+  /**
+   * Page increment step.
+   */
+  step: PropTypes.number,
+
+  /**
+   * The currently active page
+   */
+  active_page: PropTypes.number,
+
+  /**
+   * When True, this will display all numbers between `min_value` and
+   * `max_value`.
+   */
+  fully_expanded: PropTypes.bool,
+
+  /**
+   * When True, this will display a previous and next icon before and after
+   * the individual page numbers.
+   */
+  previous_next: PropTypes.bool,
+
+  /**
+   * When True, this will display a first and last icon at the beginning
+   * and end of the component.
+   */
+  first_last: PropTypes.bool
 };
 
 export default Pagination;
