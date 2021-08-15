793b3c1f36b1bd3b083e272344050f60008184b5
glsdown <52132406+glsdown@users.noreply.github.com>
2021-08-15 21:29:59 +0100

    New component Accordion (#645)
    * Accordion component and js test
    
    * Added documentation for accordion
    
    * R and Julia examples
    
    * Fixed h2 formatting issues
    
    * Added accordion folder for flake8
    
    * Update demo/Demo.js
    
    Co-authored-by: glsdown <52132406+glsdown@users.noreply.github.com>
    
    Co-authored-by: Tom Begley <tomcbegley@gmail.com>

---
+++ docs/components_page/components/accordion/collapsed.py
@@ -0,0 +1,22 @@
+import dash_bootstrap_components as dbc
+import dash_html_components as html
+
+accordion = html.Div(
+    dbc.Accordion(
+        [
+            dbc.AccordionItem(
+                "This is the content of the first section",
+                title="Item 1",
+            ),
+            dbc.AccordionItem(
+                "This is the content of the second section",
+                title="Item 2",
+            ),
+            dbc.AccordionItem(
+                "This is the content of the third section",
+                title="Item 3",
+            ),
+        ],
+        start_collapsed=True,
+    ),
+)
