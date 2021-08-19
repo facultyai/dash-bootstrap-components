11679df250a9680f391dd421c461402d673736f9
AnnMarieW <amward@fastmail.com>
2021-08-19 13:46:07 -0700

    updated index for deleted NavbarCollapse

--- src/index.js
+++ src/index.js
@@ -39,7 +39,6 @@ export {default as ModalTitle} from './components/modal/ModalTitle';
 export {default as Nav} from './components/nav/Nav';
 export {default as Navbar} from './components/nav/Navbar';
 export {default as NavbarBrand} from './components/nav/NavbarBrand';
-export {default as NavbarCollapse} from './components/nav/NavbarCollapse';
 export {default as NavbarSimple} from './components/nav/NavbarSimple';
 export {default as NavbarToggler} from './components/nav/NavbarToggler';
 export {default as NavItem} from './components/nav/NavItem';
