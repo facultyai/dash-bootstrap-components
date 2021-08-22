dee3f2a431b5b34762619dcc33799d6242089952
tcbegley <tomcbegley@gmail.com>
2021-08-22 18:38:51 +0100

    Snippet test fixes for breadcrumb

--- docs/components_page/components/breadcrumb/simple.py
+++ docs/components_page/components/breadcrumb/simple.py
@@ -2,10 +2,10 @@ import dash_bootstrap_components as dbc
 
 breadcrumb = dbc.Breadcrumb(
     items=[
-        {"label": "Docs", "href": "/docs/", "external_link": True},
+        {"label": "Docs", "href": "/docs", "external_link": True},
         {
             "label": "Components",
-            "href": "/docs/components/",
+            "href": "/docs/components",
             "external_link": True,
         },
         {"label": "Breadcrumb", "active": True},
