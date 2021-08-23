9711dac0d512bba01157dddffda0f34882967831
Gemma Down <gemmalsdown@icloud.com>
2021-08-23 14:18:48 +0100

    Added recommended hack for react testing

--- src/components/dropdownmenu/__tests__/DropdownMenuItem.test.js
+++ src/components/dropdownmenu/__tests__/DropdownMenuItem.test.js
@@ -5,6 +5,22 @@ import DropdownMenu from '../DropdownMenu';
 import DropdownMenuItem from '../DropdownMenuItem';
 
 describe('DropdownMenuItem', () => {
+  // this is just a little hack to silence a warning that we'll get until we
+  // upgrade to 16.9. See also: https://github.com/facebook/react/pull/14853
+  const originalError = console.error;
+  beforeAll(() => {
+    console.error = (...args) => {
+      if (/Warning.*not wrapped in act/.test(args[0])) {
+        return;
+      }
+      originalError.call(console, ...args);
+    };
+  });
+
+  afterAll(() => {
+    console.error = originalError;
+  });
+
   test('renders a button with class "dropdown-menu-item"', () => {
     const dropdownMenuItem = render(<DropdownMenuItem />);
 
