87be6a98c9297510117d01f9ab88d747e4a77f69
tcbegley <tomcbegley@gmail.com>
2021-08-25 08:35:42 +0100

    Fix Modal callback tests

--- docs/components_page/components/modal/size.R
+++ docs/components_page/components/modal/size.R
@@ -37,8 +37,8 @@ modal <- htmlDiv(
 )
 
 
-toggle_modal <- function(n1, n2, is_open) {
-  if (n1 > 0 | n2 > 0) {
+toggle_modal <- function(n1, is_open) {
+  if (n1 > 0) {
     return(!is_open)
   }
   return(is_open)
@@ -48,7 +48,6 @@ app$callback(
   output("modal-sm", "is_open"),
   list(
     input("open-sm", "n_clicks"),
-    input("close-sm", "n_clicks"),
     state("modal-sm", "is_open")
   ),
   toggle_modal
@@ -58,7 +57,6 @@ app$callback(
   output("modal-lg", "is_open"),
   list(
     input("open-lg", "n_clicks"),
-    input("close-lg", "n_clicks"),
     state("modal-lg", "is_open")
   ),
   toggle_modal
@@ -68,7 +66,6 @@ app$callback(
   output("modal-xl", "is_open"),
   list(
     input("open-xl", "n_clicks"),
-    input("close-xl", "n_clicks"),
     state("modal-xl", "is_open")
   ),
   toggle_modal
