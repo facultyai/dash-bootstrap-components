67dbbd9a7062044a0e7554291f4df122fdd00f6a
glsdown <gemmalsdown@icloud.com>
2021-08-30 09:32:49 +0100

    Added outline and mixed style example

---
+++ docs/components_page/components/button_group/mixed.jl
@@ -0,0 +1,9 @@
+using DashBootstrapComponents
+
+button_group = dbc_buttongroup(
+    [
+        dbc_button("Left", color="danger"),
+        dbc_button("Middle", color="warning"),
+        dbc_button("Right", color="success"),
+    ]
+)
