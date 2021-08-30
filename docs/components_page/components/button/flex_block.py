ae7e934079d0d376a6f3da6e8a45096829235a2b
glsdown <gemmalsdown@icloud.com>
2021-08-30 09:52:31 +0100

    Added example demonstrating styling of horizontal buttons

---
+++ docs/components_page/components/button/flex_block.py
@@ -0,0 +1,11 @@
+import dash_bootstrap_components as dbc
+import dash_html_components as html
+
+button = html.Div(
+    [
+        dbc.Button("Button 1", class_name="me-md-2"),
+        dbc.Button("Button 2", class_name="me-md-2"),
+        dbc.Button("Button 3"),
+    ],
+    class_name="d-grid gap-2 d-md-flex justify-content-md-end",
+)
