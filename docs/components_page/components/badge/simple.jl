dc87418959d02dd47eae818f43af7b490ac636e2
glsdown <52132406+glsdown@users.noreply.github.com>
2021-08-31 21:22:18 +0100

    Expand badge examples (#659)
    * Added positioned badge example
    
    * Changed to use text_color
    
    * Added text_color example
    
    * Format
    
    * Update Badge docs
    
    Co-authored-by: tcbegley <tomcbegley@gmail.com>

--- docs/components_page/components/badge/simple.jl
+++ docs/components_page/components/badge/simple.jl
@@ -1,6 +1,9 @@
 using DashBootstrapComponents
 
 badge = dbc_button(
-    ["Notifications", dbc_badge("4", color = "light", class_name = "ms-1 text-primary")],
+    [
+        "Notifications",
+        dbc_badge("4", color = "light", text_color = "primary", class_name = "ms-1"),
+    ],
     color = "primary",
 );
--- docs/components_page/components/badge/simple.py
+++ docs/components_page/components/badge/simple.py
@@ -3,7 +3,7 @@ import dash_bootstrap_components as dbc
 badge = dbc.Button(
     [
         "Notifications",
-        dbc.Badge("4", color="light", class_name="ms-1 text-primary"),
+        dbc.Badge("4", color="light", text_color="primary", class_name="ms-1"),
     ],
     color="primary",
 )
