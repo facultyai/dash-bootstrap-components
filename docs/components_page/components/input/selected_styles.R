1b87258ea51474ea3e1c95508b971feda7478abb
glsdown <52132406+glsdown@users.noreply.github.com>
2021-09-03 09:03:30 +0100

    Fix issues with Navbar examples (#667)
    * Add inner container to navbar example
    
    * Remove extra div in Collapse/Fade layout
    
    * Adjust navbar examples
    
    * Docs example + formatting
    
    Co-authored-by: tcbegley <tomcbegley@gmail.com>

--- docs/components_page/components/input/selected_styles.R
+++ docs/components_page/components/input/selected_styles.R
@@ -10,22 +10,22 @@ checklist <- htmlDiv(
         list(label = "Option 2", value = 2),
         list(label = "Option 3", value = 3)
       ),
-      label_checked_style=list("color" = "red"),
-      input_checked_style=list(
-          "backgroundColor" = "#fa7268",
-          "borderColor" = "#ea6258"
+      label_checked_style = list("color" = "red"),
+      input_checked_style = list(
+        "backgroundColor" = "#fa7268",
+        "borderColor" = "#ea6258"
       )
     ),
     htmlHr(),
     dbcRadioItems(
-      id="radio-selected-style",
+      id = "radio-selected-style",
       options = list(
         list(label = "Option 1", value = 1),
         list(label = "Option 2", value = 2),
         list(label = "Option 3", value = 3)
       ),
-      label_checked_class_name="text-success",
-      input_checked_class_name="border border-success bg-success"
+      label_checked_class_name = "text-success",
+      input_checked_class_name = "border border-success bg-success"
     )
   )
 )
