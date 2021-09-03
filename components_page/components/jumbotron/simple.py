26bdedee41dda28f993d519cd7f94adfe7f67fce
glsdown <52132406+glsdown@users.noreply.github.com>
2021-09-03 21:36:44 +0100

    Added jumbotron example (#671)
    * Added jumbotron example
    
    * Format
    
    * Update Jumbotron language
    
    Co-authored-by: tcbegley <tomcbegley@gmail.com>

---
+++ components_page/components/jumbotron/simple.py
@@ -0,0 +1,26 @@
+import dash_bootstrap_components as dbc
+import dash_html_components as html
+
+jumbotron = html.Div(
+    dbc.Container(
+        [
+            html.H1("Jumbotron", class_name="display-3"),
+            html.P(
+                "Use Containers to create a jumbotron to call attention to "
+                "featured content or information.",
+                class_name="lead",
+            ),
+            html.Hr(class_name="my-2"),
+            html.P(
+                "Use utility classes for typography and spacing to suit the "
+                "larger container."
+            ),
+            html.P(
+                dbc.Button("Learn more", color="primary"), class_name="lead"
+            ),
+        ],
+        fluid=True,
+        class_name="py-3",
+    ),
+    class_name="p-3 bg-light rounded-3",
+)
