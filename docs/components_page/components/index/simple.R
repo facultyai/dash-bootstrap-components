0c193a7203059e85160217c49d08ad34e6077504
tcbegley <tomcbegley@gmail.com>
2021-09-04 21:54:32 +0100

    Fix issues in R snippet

--- docs/components_page/components/index/simple.R
+++ docs/components_page/components/index/simple.R
@@ -3,8 +3,8 @@ library(dashHtmlComponents)
 
 layout <- dbcAlert(
   paste(
-    "This is an example of a component being used in the wild. ",
-    "Below me, you can find the code used to create me.",
+    "This is an example of a component being used in the wild.",
+    "Below me, you can find the code used to create me."
   ),
-  color = "info",
+  color = "info"
 )
