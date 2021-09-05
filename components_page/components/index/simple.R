22f21b0570a00a044ea92ff15c7b19a2769935ea
Tom Begley <tomcbegley@gmail.com>
2021-09-05 07:37:28 +0100

    Update component landing page (#677)
    * Update component landing page
    
    * Fix issues in R snippet

---
+++ components_page/components/index/simple.R
@@ -0,0 +1,10 @@
+library(dashBootstrapComponents)
+library(dashHtmlComponents)
+
+layout <- dbcAlert(
+  paste(
+    "This is an example of a component being used in the wild.",
+    "Below me, you can find the code used to create me."
+  ),
+  color = "info"
+)
