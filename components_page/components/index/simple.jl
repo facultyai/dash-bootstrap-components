c8e299d1e2c191824a23b9b11e8b6b460cc3f170
Tom Begley <tomcbegley@gmail.com>
2021-09-11 08:28:40 +0100

    Improved component landing page (#687)
    * Update code-example parser
    
    * Updated component overview page
    
    * Update component landing page language
    
    * Fix bug in R snippet
    
    * Add test

--- components_page/components/index/simple.jl
+++ components_page/components/index/simple.jl
@@ -1,7 +1,22 @@
-using DashBootstrapComponents, DashHtmlComponents
+# 1. Import Dash
+using Dash, DashBootstrapComponents
 
-layout = dbc_alert(
-    "This is an example of a component being used in the wild. " *
-    "Below me, you can find the code used to create me.",
-    color = "info",
+# 2. Create a Dash app instance
+app = dash(external_stylesheets = [dbc_themes.BOOTSTRAP])
+
+# 3. Add the example to the app's layout
+# First we copy the snippet from the docs
+badge = dbc_button(
+    [
+        "Notifications",
+        dbc_badge("4", color = "light", text_color = "primary", className = "ms-1"),
+    ],
+    color = "primary",
 )
+
+# Then we incorporate the snippet into our layout.
+# This example keeps it simple and just wraps it in a Container
+app.layout = dbc_container(badge, fluid = true)
+
+# 5. Start the Dash server
+run_server(app, "0.0.0.0", 8050)
--- components_page/markdown_parser.py
+++ components_page/markdown_parser.py
@@ -5,7 +5,11 @@ import dash_bootstrap_components as dbc
 import markdown
 from dash import dcc, html
 
-from .helpers import ExampleContainer, load_source_with_environment
+from .helpers import (
+    ExampleContainer,
+    HighlightedSource,
+    load_source_with_environment,
+)
 
 __all__ = ["parse"]
 
@@ -91,12 +95,17 @@ def _safe_load_source(source_path, ext):
         return None
 
 
-def _parse_code_example(data):
-    source_path, language = data.split(":")
-    source = (HERE / source_path).read_text().strip()
+def _parse_code_example(filename):
+    source_path = HERE / filename
+    py_source = (HERE / source_path).read_text().strip()
+    r_source = _safe_load_source(source_path, "R")
+    jl_source = _safe_load_source(source_path, "jl")
+
     return html.Div(
-        dcc.Markdown(f"```{language}\n{source}\n```"),
-        className="source-container",
+        HighlightedSource(
+            py_source, r_source, jl_source, className="pb-0 card-header"
+        ),
+        className="border source-container rounded",
     )
 
 
