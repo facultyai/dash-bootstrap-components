1fa6f0d80487643794fd996e0695ff28be67a248
Tom Begley <tomcbegley@gmail.com>
2021-11-27 16:20:32 +0000

    Prerelease/1.0.1 rc4 (#782)
    * Prerelease 1.0.1-rc4
    
    * Update Python requirements in setup.py

--- setup.py
+++ setup.py
@@ -50,5 +50,5 @@ setup(
         "Programming Language :: Python :: 3.9",
     ],
     extras_require={"pandas": ["numpy", "pandas"]},
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4",
+    python_requires=">=3.6, <4",
 )
