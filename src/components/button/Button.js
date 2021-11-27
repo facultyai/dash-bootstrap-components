e46e1066d784bf1606169f835a382625b47c5937
Tom Begley <tomcbegley@gmail.com>
2021-11-27 16:06:53 +0000

    Allow target to be set on Button with href (#781)

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -44,17 +44,22 @@ const Button = props => {
   const useLink = href && !disabled;
   otherProps[useLink ? 'preOnClick' : 'onClick'] = onClick || incrementClicks;
 
+  if (useLink) {
+    // react-bootstrap strips out target prop if tag isn't an anchor element,
+    // so we pass it through under a different name
+    otherProps['linkTarget'] = target;
+  }
+
   return (
     <RBButton
       as={useLink ? Link : 'button'}
       variant={outline ? `outline-${color}` : color}
-      type={useLink ? null : type}
-      target={useLink ? target : null}
-      href={disabled ? null : href}
+      type={useLink ? undefined : type}
+      href={disabled ? undefined : href}
       disabled={disabled}
-      download={useLink ? download : null}
-      name={useLink ? null : name}
-      value={useLink ? null : value}
+      download={useLink ? download : undefined}
+      name={useLink ? undefined : name}
+      value={useLink ? undefined : value}
       className={class_name || className}
       {...omit(['n_clicks_timestamp'], otherProps)}
       data-dash-is-loading={
