87c1120cfb5e85af09d99e2605e953cfc7c82a8e
tcbegley <tomcbegley@gmail.com>
2021-12-12 17:37:46 +0000

    R release 1.0.2

--- R/dbcTooltip.R
+++ R/dbcTooltip.R
@@ -1,8 +1,8 @@
 # AUTO GENERATED FILE - DO NOT EDIT
 
-dbcTooltip <- function(children=NULL, id=NULL, className=NULL, class_name=NULL, delay=NULL, flip=NULL, key=NULL, loading_state=NULL, placement=NULL, style=NULL, target=NULL) {
+dbcTooltip <- function(children=NULL, id=NULL, autohide=NULL, className=NULL, class_name=NULL, delay=NULL, flip=NULL, key=NULL, loading_state=NULL, placement=NULL, style=NULL, target=NULL) {
     
-    props <- list(children=children, id=id, className=className, class_name=class_name, delay=delay, flip=flip, key=key, loading_state=loading_state, placement=placement, style=style, target=target)
+    props <- list(children=children, id=id, autohide=autohide, className=className, class_name=class_name, delay=delay, flip=flip, key=key, loading_state=loading_state, placement=placement, style=style, target=target)
     if (length(props) > 0) {
         props <- props[!vapply(props, is.null, logical(1))]
     }
@@ -10,7 +10,7 @@ dbcTooltip <- function(children=NULL, id=NULL, className=NULL, class_name=NULL, 
         props = props,
         type = 'Tooltip',
         namespace = 'dash_bootstrap_components',
-        propNames = c('children', 'id', 'className', 'class_name', 'delay', 'flip', 'key', 'loading_state', 'placement', 'style', 'target'),
+        propNames = c('children', 'id', 'autohide', 'className', 'class_name', 'delay', 'flip', 'key', 'loading_state', 'placement', 'style', 'target'),
         package = 'dashBootstrapComponents'
         )
 
