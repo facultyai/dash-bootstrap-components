216ca5ed43a0bbd31e13f54c79ddf3de5a69c4a3
tcbegley <tomcbegley@gmail.com>
2022-04-03 17:03:06 +0100

    R release 1.1.0

--- R/dbcAccordion.R
+++ R/dbcAccordion.R
@@ -1,9 +1,9 @@
 # AUTO GENERATED FILE - DO NOT EDIT
 
 #' @export
-dbcAccordion <- function(children=NULL, id=NULL, active_item=NULL, className=NULL, class_name=NULL, flush=NULL, key=NULL, loading_state=NULL, persisted_props=NULL, persistence=NULL, persistence_type=NULL, start_collapsed=NULL, style=NULL) {
+dbcAccordion <- function(children=NULL, id=NULL, active_item=NULL, always_open=NULL, className=NULL, class_name=NULL, flush=NULL, key=NULL, loading_state=NULL, persisted_props=NULL, persistence=NULL, persistence_type=NULL, start_collapsed=NULL, style=NULL) {
     
-    props <- list(children=children, id=id, active_item=active_item, className=className, class_name=class_name, flush=flush, key=key, loading_state=loading_state, persisted_props=persisted_props, persistence=persistence, persistence_type=persistence_type, start_collapsed=start_collapsed, style=style)
+    props <- list(children=children, id=id, active_item=active_item, always_open=always_open, className=className, class_name=class_name, flush=flush, key=key, loading_state=loading_state, persisted_props=persisted_props, persistence=persistence, persistence_type=persistence_type, start_collapsed=start_collapsed, style=style)
     if (length(props) > 0) {
         props <- props[!vapply(props, is.null, logical(1))]
     }
@@ -11,7 +11,7 @@ dbcAccordion <- function(children=NULL, id=NULL, active_item=NULL, className=NUL
         props = props,
         type = 'Accordion',
         namespace = 'dash_bootstrap_components',
-        propNames = c('children', 'id', 'active_item', 'className', 'class_name', 'flush', 'key', 'loading_state', 'persisted_props', 'persistence', 'persistence_type', 'start_collapsed', 'style'),
+        propNames = c('children', 'id', 'active_item', 'always_open', 'className', 'class_name', 'flush', 'key', 'loading_state', 'persisted_props', 'persistence', 'persistence_type', 'start_collapsed', 'style'),
         package = 'dashBootstrapComponents'
         )
 
