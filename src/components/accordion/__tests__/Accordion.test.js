b87c349f5b299cc56c1a7b762e341331abfc34a8
Gemma Down <52132406+glsdown@users.noreply.github.com>
2022-04-03 10:38:53 +0100

    Add Accordion always_open property (#840)
    * Bump react-bootstrap version
    
    * Add always_open prop to Accordion
    
    * Add js tests for Accordion always_open
    
    * Add always_open to Accordion docs
    
    * Add always_open callback doc test
    
    * Remove unnecessary imports
    
    * Add callback file to flake8 ignore files
    
    * Call the second callback test
    
    * Bump react-bootstrap version
    
    * Add always_open prop to Accordion
    
    * Add js tests for Accordion always_open
    
    * Add always_open to Accordion docs
    
    * Add always_open callback doc test
    
    * Remove unnecessary imports
    
    * Add callback file to flake8 ignore files
    
    * Call the second callback test
    
    * Fix some issues with tests
    
    * Remove always_open example in R
    
    * Clean up Accordion source
    
    * Fix accordion tests
    
    * Remove note about R
    
    * Revert debugging change to tests
    
    Co-authored-by: tcbegley <tomcbegley@gmail.com>

--- src/components/accordion/__tests__/Accordion.test.js
+++ src/components/accordion/__tests__/Accordion.test.js
@@ -150,4 +150,162 @@ describe('Accordion', () => {
       accordionItems.children[1].querySelector('div.accordion-collapse')
     ).not.toHaveClass('show');
   });
+
+  test('keeps item open with "always_open" prop', () => {
+    const mockSetProps = jest.fn();
+
+    const {container} = render(
+      <Accordion setProps={mockSetProps} always_open start_collapsed>
+        <AccordionItem title="item-title-1">item-content-1</AccordionItem>
+        <AccordionItem title="item-title-2">item-content-2</AccordionItem>
+      </Accordion>
+    );
+
+    const accordionItems = container.querySelector('div.accordion');
+
+    // Check none of the items are open
+    expect(
+      accordionItems.children[0].querySelector('div.accordion-collapse')
+    ).not.toHaveClass('show');
+    expect(
+      accordionItems.children[1].querySelector('div.accordion-collapse')
+    ).not.toHaveClass('show');
+
+    // Click on the second item
+    userEvent.click(
+      accordionItems.children[1].querySelector('h2.accordion-header > button')
+    );
+    expect(mockSetProps.mock.calls).toHaveLength(1);
+
+    // Allow the click to take effect
+    jest.runAllTimers();
+
+    // Check just the second item is open
+    expect(
+      accordionItems.children[0].querySelector('div.accordion-collapse')
+    ).not.toHaveClass('show');
+    expect(
+      accordionItems.children[1].querySelector('div.accordion-collapse')
+    ).toHaveClass('show');
+
+    // Click on the first item
+    userEvent.click(
+      accordionItems.children[0].querySelector('h2.accordion-header > button')
+    );
+    expect(mockSetProps.mock.calls).toHaveLength(2);
+    // Allow the click to take effect
+    jest.runAllTimers();
+
+    // Check that the first child is now open, and the second remains open
+    expect(
+      accordionItems.children[0].querySelector('div.accordion-collapse')
+    ).toHaveClass('show');
+    expect(
+      accordionItems.children[1].querySelector('div.accordion-collapse')
+    ).toHaveClass('show');
+  });
+
+  test('allow multiple items open with "always_open" prop', () => {
+    const {container} = render(
+      <Accordion always_open active_item={['item-0', 'item-1']}>
+        <AccordionItem title="item-title-1">item-content-1</AccordionItem>
+        <AccordionItem title="item-title-2">item-content-2</AccordionItem>
+      </Accordion>
+    );
+
+    const accordionItems = container.querySelector('div.accordion');
+
+    // Check both items open
+    expect(
+      accordionItems.children[0].querySelector('div.accordion-collapse')
+    ).toHaveClass('show');
+    expect(
+      accordionItems.children[1].querySelector('div.accordion-collapse')
+    ).toHaveClass('show');
+  });
+
+  test('tracks most recently clicked item with "active_item" prop when always_open', () => {
+    const mockSetProps = jest.fn();
+    const {container, rerender} = render(
+      <Accordion setProps={mockSetProps} active_item={['item-0']} always_open>
+        <AccordionItem title="item-title-1">item-content-1</AccordionItem>
+        <AccordionItem title="item-title-2">item-content-2</AccordionItem>
+        <AccordionItem title="item-title-3">item-content-3</AccordionItem>
+      </Accordion>
+    );
+
+    let accordionItems = container.querySelector('div.accordion');
+
+    // Expect first item open
+    expect(
+      accordionItems.children[0].querySelector('div.accordion-collapse')
+    ).toHaveClass('show');
+    expect(
+      accordionItems.children[1].querySelector('div.accordion-collapse')
+    ).not.toHaveClass('show');
+    expect(
+      accordionItems.children[2].querySelector('div.accordion-collapse')
+    ).not.toHaveClass('show');
+
+    // Click the middle option
+    userEvent.click(
+      accordionItems.children[1].querySelector('h2.accordion-header > button')
+    );
+    expect(mockSetProps.mock.calls).toHaveLength(1);
+
+    rerender(
+      <Accordion
+        always_open
+        setProps={mockSetProps}
+        {...mockSetProps.mock.calls[0][0]}
+      >
+        <AccordionItem title="item-title-1">item-content-1</AccordionItem>
+        <AccordionItem title="item-title-2">item-content-2</AccordionItem>
+        <AccordionItem title="item-title-3">item-content-3</AccordionItem>
+      </Accordion>
+    );
+    jest.runAllTimers();
+
+    // Check first option stayed open, middle option now open but third still
+    // closed
+    expect(
+      accordionItems.children[0].querySelector('div.accordion-collapse')
+    ).toHaveClass('show');
+    expect(
+      accordionItems.children[1].querySelector('div.accordion-collapse')
+    ).toHaveClass('show');
+    expect(
+      accordionItems.children[2].querySelector('div.accordion-collapse')
+    ).not.toHaveClass('show');
+
+    // clicking on 1st item closes it, but keeps second item open and 3rd closed
+    userEvent.click(
+      accordionItems.children[0].querySelector('h2.accordion-header > button')
+    );
+    expect(mockSetProps.mock.calls).toHaveLength(2);
+
+    rerender(
+      <Accordion
+        always_open
+        setProps={mockSetProps}
+        {...mockSetProps.mock.calls[1][0]}
+      >
+        <AccordionItem title="item-title-1">item-content-1</AccordionItem>
+        <AccordionItem title="item-title-2">item-content-2</AccordionItem>
+        <AccordionItem title="item-title-3">item-content-3</AccordionItem>
+      </Accordion>
+    );
+    jest.runAllTimers();
+
+    // Check that 1 and 3 now closed, and 2 is open
+    expect(
+      accordionItems.children[0].querySelector('div.accordion-collapse')
+    ).not.toHaveClass('show');
+    expect(
+      accordionItems.children[1].querySelector('div.accordion-collapse')
+    ).toHaveClass('show');
+    expect(
+      accordionItems.children[2].querySelector('div.accordion-collapse')
+    ).not.toHaveClass('show');
+  });
 });
