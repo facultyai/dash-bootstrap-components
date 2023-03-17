1fd446231acd84f5380d5a6218b729d9f77d18b4
Tom Begley <tomcbegley@gmail.com>
2023-03-17 21:49:32 +0000

    Update out of date CDN links (#950)

--- jl/icons.jl
+++ jl/icons.jl
@@ -1,6 +1,6 @@
 export dbc_icons
 
 dbc_icons = (
-    BOOTSTRAP = "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css",
-    FONT_AWESOME = "https://use.fontawesome.com/releases/v6.1.1/css/all.css",
+    BOOTSTRAP = "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css",
+    FONT_AWESOME = "https://use.fontawesome.com/releases/v6.3.0/css/all.css",
 )
