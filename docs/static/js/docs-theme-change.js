efb963f65500eee4d4395cc566a1c5bebcdc630a
AnnMarueW <amward@fastmail.com>
2023-10-16 12:31:33 -0700

    docs color mode theme switch

---
+++ docs/static/js/docs-theme-change.js
@@ -0,0 +1,30 @@
+const getStoredTheme = () => localStorage.getItem('theme')
+const lightIcon = "bi bi-sun-fill"
+const darkIcon = "bi bi-moon-fill"
+
+
+const setIcon = theme => {
+  icon = theme == "light" ? lightIcon : darkIcon
+  document.getElementById('theme-icon').className = icon
+}
+
+const handleThemeChange = () => {
+  getStoredTheme() == 'dark' ? setTheme("light") : setTheme("dark")
+}
+
+const setTheme = theme => {
+  document.documentElement.setAttribute('data-bs-theme', theme)
+  localStorage.setItem('theme', theme)
+  setIcon(theme)
+}
+
+// icon needs to be set after page is loaded
+const setInitialIcon = () => {
+  getStoredTheme() == 'dark' ? setIcon("dark") : setIcon("light")
+}
+window.onload = (event) => {
+  setInitialIcon()
+};
+
+
+
