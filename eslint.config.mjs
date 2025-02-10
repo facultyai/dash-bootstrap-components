00e618f19177f398b46abe1db64efb6362abbc89
Tom Begley <tomcbegley@gmail.com>
2025-02-10 19:13:54 +0000

    Configure import formatting / linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,30 @@
-import globals from 'globals';
 import pluginJs from '@eslint/js';
 import pluginJest from 'eslint-plugin-jest';
 import pluginReact from 'eslint-plugin-react';
+import unusedImports from 'eslint-plugin-unused-imports';
+import globals from 'globals';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
     files: ['**/*.{js,mjs,cjs,jsx}'],
-    plugins: {jest: pluginJest},
+    settings: {react: {version: 'detect'}},
+    plugins: {jest: pluginJest, 'unused-imports': unusedImports},
     languageOptions: {
       globals: pluginJest.environments.globals.globals
     },
     rules: {
+      'no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_'
+        }
+      ],
       'jest/no-disabled-tests': 'warn',
       'jest/no-focused-tests': 'error',
       'jest/no-identical-title': 'error',
